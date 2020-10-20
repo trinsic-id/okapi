4aeeb856b69cd6395f382688f29c70879c84efaf
Tomislav Markovski <tomislav@streetcred.id>
2020-10-20 19:35:06 -0400

    Client and server implementation

---
+++ src/Server/DIDCommService.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using Common;
+using DIDCommMessaging;
+using Google.Protobuf;
+using Grpc.Core;
+
+namespace Server
+{
+    public class DIDCommService : DIDComm.DIDCommBase
+    {
+        public override Task<DidcommMessage> Unary(DidcommMessage request, ServerCallContext context)
+        {
+            if (request.Type != TypeUrls.BasicMessage)
+            {
+                throw new Exception("Unsupported message type");
+            }
+
+            // Extract request message
+            var basiMessage = new BasicMessage();
+            basiMessage.MergeFrom(request.Body);
+
+            // Send back response
+            return Task.FromResult(new DidcommMessage
+            {
+                Id = "1",
+                From = "did:key:alice",
+                Created = DateTimeOffset.Now.ToUnixTimeSeconds(),
+                Body = new BasicMessage
+                {
+                    Text = $"You said (unary): {basiMessage.Text}"
+                }.ToByteString()
+            });
+        }
+
+        public override async Task ServerStreaming(DidcommMessage request, IServerStreamWriter<DidcommMessage> responseStream, ServerCallContext context)
+        {
+            if (request.Type != TypeUrls.BasicMessage)
+            {
+                throw new Exception("Unsupported message type");
+            }
+
+            // Extract request message
+            var basiMessage = new BasicMessage();
+            basiMessage.MergeFrom(request.Body);
+
+            await responseStream.WriteAsync(new DidcommMessage
+            {
+                Id = "1",
+                From = "did:key:bob",
+                Created = DateTimeOffset.Now.ToUnixTimeSeconds(),
+                Body = new BasicMessage
+                {
+                    Text = "You said (streamed):"
+                }.ToByteString()
+            });
+
+            // Send back response
+            foreach (var letter in basiMessage.Text)
+            {
+                await responseStream.WriteAsync(new DidcommMessage
+                {
+                    Id = "1",
+                    From = "did:key:bob",
+                    Created = DateTimeOffset.Now.ToUnixTimeSeconds(),
+                    Body = new BasicMessage
+                    {
+                        Text = $"{letter}"
+                    }.ToByteString()
+                });
+            }
+        }
+    }
+}
---
+++ src/Server/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace Server
+{
+    class Program
+    {
+        static Task Main(string[] args) => CreateDefaultBuilder(args).Build().RunAsync();
+
+        private static IWebHostBuilder CreateDefaultBuilder(string[] args) => WebHost.CreateDefaultBuilder(args)
+            .UseStartup<Startup>();
+    }
+}
