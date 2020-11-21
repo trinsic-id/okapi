ecda41fe10a5b45ee25448bbfb0b266d9060199e
Tomislav Markovski <tomislav@streetcred.id>
2020-11-21 01:25:51 +0000

    Merged PR 1589: Python + Go POC
    Related work items: #3619, #3618

--- include/didcommgrpc.h
+++ include/didcommgrpc.h
@@ -2,26 +2,26 @@
 
 typedef struct {
     int64_t len;
-    uint8_t *_Nullable data;
+    uint8_t * _Nullable data;
 } ByteBuffer;
 
 typedef struct {
     int32_t code;
-    char* message; /* note: nullable */
+    char * _Nullable message; /* note: nullable */
 } ExternError;
 
-void didcomm_string_free(char* string);
+void didcomm_string_free(char* _Nonnull string);
 
 void didcomm_byte_buffer_free(ByteBuffer data);
 
-int32_t didcomm_generate_key(ByteBuffer request, ByteBuffer *response, ExternError *err);
+int32_t didcomm_generate_key(ByteBuffer request, ByteBuffer * _Nullable response, ExternError * _Nullable err);
 
-int32_t didcomm_convert_key(ByteBuffer request, ByteBuffer *response, ExternError *err);
+int32_t didcomm_convert_key(ByteBuffer request, ByteBuffer * _Nullable response, ExternError * _Nullable err);
 
-int32_t didcomm_pack(ByteBuffer request, ByteBuffer *response, ExternError *err);
+int32_t didcomm_pack(ByteBuffer request, ByteBuffer * _Nullable response, ExternError * _Nullable err);
 
-int32_t didcomm_unpack(ByteBuffer request, ByteBuffer *response, ExternError *err);
+int32_t didcomm_unpack(ByteBuffer request, ByteBuffer * _Nullable response, ExternError * _Nullable err);
 
-int32_t didcomm_sign(ByteBuffer request, ByteBuffer *response, ExternError *err);
+int32_t didcomm_sign(ByteBuffer request, ByteBuffer * _Nullable response, ExternError * _Nullable err);
 
-int32_t didcomm_verify(ByteBuffer request, ByteBuffer *response, ExternError *err);
+int32_t didcomm_verify(ByteBuffer request, ByteBuffer * _Nullable response, ExternError * _Nullable err);
