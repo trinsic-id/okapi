67e2d558f241d9c0b96b3f0b58d3422730bd96b4
KandareJ <jace.kandare@yahoo.com>
2020-12-10 13:48:33 -0700

    Added Jar artifact with .dylib

--- java/src/DIDComm/Messaging/gRPC/NativeMethods.java
+++ java/src/DIDComm/Messaging/gRPC/NativeMethods.java
@@ -1,5 +1,10 @@
 package DIDComm.Messaging.gRPC;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 public class NativeMethods {
     public static native byte[] didkey_generate(byte[] request);
     public static native byte[] didkey_convert(byte[] request);
@@ -9,6 +14,38 @@ public class NativeMethods {
     public static native byte[] didcomm_verify(byte[] request);
 
     static {
-        System.loadLibrary("didcommgrpc");
+        String path = new File("").getAbsolutePath() + "/resources/";
+        String filename = System.mapLibraryName("didcommgrpc");
+        System.out.println("Searching for " + filename);
+        System.out.println("FOUND OS: " + System.getProperty("os.name"));
+
+
+        try {
+            System.load(path + filename);
+        } catch (UnsatisfiedLinkError linkError) {
+            try {
+                InputStream inputStream = null;
+                OutputStream outputStream = null;
+                byte[] buffer = new byte[1024];
+                int read = -1;
+
+                File directory = new File(path);
+                if (!directory.exists()) directory.mkdir();
+
+                inputStream = NativeMethods.class.getResourceAsStream("/" + filename);
+                File fileOut = new File(path + filename);
+                outputStream = new FileOutputStream(fileOut);
+
+                while((read = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, read);
+                }
+
+                System.load(fileOut.toString());
+
+            } catch (Exception e) {
+                System.out.println("Could not load library: " + filename);
+                e.printStackTrace();
+            }
+        }
     }
 }
