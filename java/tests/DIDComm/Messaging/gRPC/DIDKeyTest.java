89b8d1da1a5a1dd5b9d98d93271fe28c65e03e31
Jace Kandare <jace.kandare@yahoo.com>
2021-01-20 17:19:20 -0700

    Update Java wrapper, examples, and tests

--- java/tests/DIDComm/Messaging/gRPC/DIDKeyTest.java
+++ java/tests/DIDComm/Messaging/gRPC/DIDKeyTest.java
@@ -1,6 +1,10 @@
 package DIDComm.Messaging.gRPC;
-/*
+
+
+import DIDComm.Messaging.Proto.API;
 import org.junit.Test;
+
+import java.util.Base64;
 
 import static org.junit.Assert.*;
 
@@ -9,14 +13,16 @@ public class DIDKeyTest {
     @Test
     public void testGenerateKeyNoSeed() {
         try {
-            API.GenerateKeyRequest request = API.GenerateKeyRequest.newBuilder().setKeyType(API.KeyType.ed25519).build();
+            API.GenerateKeyRequest request = API.GenerateKeyRequest.newBuilder().setKeyType(API.Crv.Ed25519).build();
             API.GenerateKeyResponse response = DIDKey.Generate(request);
+            Base64.Decoder decoder = Base64.getDecoder();
+
 
             assertNotNull("Response should not be null", response);
             assertNotNull("Key should not be null", response.getKey());
-            assertEquals(API.KeyType.ed25519, response.getKey().getKeyType());
-            assertEquals(32, response.getKey().getPublicKey().size());
-            assertEquals(32, response.getKey().getSecretKey().size());
+            assertEquals(API.Crv.Ed25519, response.getKey().getCrv());
+            assertEquals(32, decoder.decode(response.getKey().getX()).length + decoder.decode(response.getKey().getY()).length);
+            assertEquals(32, decoder.decode(response.getKey().getD()).length);
 
         } catch (Exception e) {
             assertNotNull("Exception: " + e.getMessage(), null);
@@ -28,7 +34,7 @@ public class DIDKeyTest {
     public void testGenerateKeyThrowsInvalidSeedSize() {
         boolean thrown = false;
         try {
-            API.GenerateKeyRequest request = API.GenerateKeyRequest.newBuilder().setKeyType(API.KeyType.UNRECOGNIZED).build();
+            API.GenerateKeyRequest request = API.GenerateKeyRequest.newBuilder().setKeyType(API.Crv.UNRECOGNIZED).build();
             DIDKey.Generate(request);
         } catch(Exception e) { // TODO: Not sure this is catching the right exception. Or what the right exception is.
             thrown = true;
@@ -37,4 +43,4 @@ public class DIDKeyTest {
         assertTrue("Exception not thrown with invalid seed size", thrown);
     }
 
-}*/
+}
