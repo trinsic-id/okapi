8765dac86def07e5bc36703de5d7b7ce9fcfc4b5
KandareJ <39170060+KandareJ@users.noreply.github.com>
2021-01-25 15:34:14 -0700

    Update Dotnet and Android wrappers (#15)
    * Update libs
    
    * dotnet refactor
    
    * Android wrapper update
    
    * Update dotnet sample
    
    * Update gitignore
    
    * Remove unneeded files

--- dotnet/Samples/Greeter/Common/TestKeys.cs
+++ dotnet/Samples/Greeter/Common/TestKeys.cs
@@ -8,18 +8,20 @@ namespace Common
     public static class Alice
     {
         const string KeyId = "did:key:z6LSduVJgrLfZwnDtASwGa4VMjvTAHYtpHrD7vupLmxyBnJ4#z6LSduVJgrLfZwnDtASwGa4VMjvTAHYtpHrD7vupLmxyBnJ4";
-
-        public static Key SecretKey = new Key
+        public static JsonWebKey SecretKey = new JsonWebKey
         {
             KeyId = KeyId,
-            KeyType = KeyType.X25519,
-            SecretKey = ByteString.CopyFrom(Multibase.Base58.Decode("BEyxtiSbfeXZxBmgg9et5oo3nYMh11iQ8TVvJSrKJQzQ"))
+            Kty = KeyType.Okp,
+            Crv = Crv.X25519,
+            D = Multibase.Base64.Encode(Multibase.Base58.Decode("BEyxtiSbfeXZxBmgg9et5oo3nYMh11iQ8TVvJSrKJQzQ"))
         };
-        public static Key PublicKey = new Key
+        public static JsonWebKey PublicKey = new JsonWebKey
         {
             KeyId = KeyId,
-            KeyType = KeyType.X25519,
-            PublicKey = ByteString.CopyFrom(Multibase.Base58.Decode("3EK9AYXoUV4Unn5AjvYY39hyK91n7gg4ExC8rKKSUQXJ"))
+            Kty = KeyType.Okp,
+            Crv = Crv.X25519,
+            X = Multibase.Base64.Encode(Multibase.Base58.Decode("3EK9AYXoUV4Unn5AjvYY39hyK91n7gg4ExC8rKKSUQXJ")),
+            Y = ""
         };
     }
 
@@ -27,17 +29,20 @@ namespace Common
     {
         const string KeyId = "did:key:z6LSkNeNYQ7W1wFb1Smrxir7vTKKLMe1CgYXj8g26oVW7CjX#z6LSkNeNYQ7W1wFb1Smrxir7vTKKLMe1CgYXj8g26oVW7CjX";
 
-        public static Key SecretKey = new Key
+        public static JsonWebKey SecretKey = new JsonWebKey
         {
             KeyId = KeyId,
-            KeyType = KeyType.X25519,
-            SecretKey = ByteString.CopyFrom(Multibase.Base58.Decode("G5UdbKAt8ux4CgFySveHQLbjY9GJqxsXhFuFkDtQVuSo"))
+            Kty = KeyType.Okp,
+            Crv = Crv.X25519,
+            D = Multibase.Base64.Encode(Multibase.Base58.Decode("G5UdbKAt8ux4CgFySveHQLbjY9GJqxsXhFuFkDtQVuSo"))
         };
-        public static Key PublicKey = new Key
+        public static JsonWebKey PublicKey = new JsonWebKey
         {
             KeyId = KeyId,
-            KeyType = KeyType.X25519,
-            PublicKey = ByteString.CopyFrom(Multibase.Base58.Decode("9hUD26JdvUXqv4Q6S5LAbs6qVD6tW5NNr9xLcLqyPpxm"))
+            Kty = KeyType.Okp,
+            Crv = Crv.X25519,
+            X = Multibase.Base64.Encode(Multibase.Base58.Decode("9hUD26JdvUXqv4Q6S5LAbs6qVD6tW5NNr9xLcLqyPpxm")),
+            Y = ""
         };
     }
 }
--- dotnet/Tests/DIDComm.Messaging.Tests/KeyTests.cs
+++ dotnet/Tests/DIDComm.Messaging.Tests/KeyTests.cs
@@ -12,30 +12,38 @@ namespace DIDComm.Messaging.Tests
         [Fact(DisplayName = "Generate new key with empty seed")]
         public void TestGenerateKeyNoSeed()
         {
-            var response = DIDKey.Generate(new GenerateKeyRequest { KeyType = KeyType.Ed25519 });
+            var response = DIDKey.Generate(new GenerateKeyRequest { KeyType = Crv.Ed25519 });
+            byte[] x = System.Convert.FromBase64String(response.Key.X);
+            byte[] y = System.Convert.FromBase64String(response.Key.Y);
+            byte[] publicKey = new byte[x.Length + y.Length];
+
+            System.Buffer.BlockCopy(x, 0, publicKey, 0, x.Length);
+            System.Buffer.BlockCopy(y, 0, publicKey, x.Length, y.Length);
 
             response.Should().NotBeNull();
             response.Key.Should().NotBeNull();
-            response.Key.KeyType.Should().Be(KeyType.Ed25519);
-            response.Key.PublicKey.Should().NotBeNull().And.HaveCount(32);
-            response.Key.SecretKey.Should().NotBeNull().And.HaveCount(32);
+            response.Key.Crv.Should().Be(Crv.Ed25519);
+            publicKey.Should().NotBeNull().And.HaveCount(32);
+            System.Convert.FromBase64String(response.Key.D).Should().NotBeNull().And.HaveCount(32);
         }
 
+        
         [Fact(DisplayName = "Generate new key throws for invalid key type")]
         public void TestGenerateKeyThrowsInvalidSeedSize()
         {
             Assert.Throws<DIDCommException>(() =>
                 _ = DIDKey.Generate(new GenerateKeyRequest
                 {
-                    KeyType = (KeyType)(-1)
+                    KeyType = (Crv)(-1)
                 })
             );
         }
+      
 
         [Theory(DisplayName = "Generate new key from seed")]
-        [InlineData(KeyType.Ed25519, "4f66b355aa7b0980ff901f2295b9c562ac3061be4df86703eb28c612faae6578", "6fioC1zcDPyPEL19pXRS2E4iJ46zH7xP6uSgAaPdwDrx")]
-        [InlineData(KeyType.X25519, "9b29d42b38ddd52ed39c0ff70b39572a6eb9b3cac201918dc6d6a84b4c88d2a5", "3EK9AYXoUV4Unn5AjvYY39hyK91n7gg4ExC8rKKSUQXJ")]
-        public void TestGenerateKeyFromSeed(KeyType keyType, string seed, string publicKey)
+        [InlineData(Crv.Ed25519, "4f66b355aa7b0980ff901f2295b9c562ac3061be4df86703eb28c612faae6578", "6fioC1zcDPyPEL19pXRS2E4iJ46zH7xP6uSgAaPdwDrx")]
+        [InlineData(Crv.X25519, "9b29d42b38ddd52ed39c0ff70b39572a6eb9b3cac201918dc6d6a84b4c88d2a5", "3EK9AYXoUV4Unn5AjvYY39hyK91n7gg4ExC8rKKSUQXJ")]
+        public void TestGenerateKeyFromSeed(Crv keyType, string seed, string publicKey)
         {
             var response = DIDKey.Generate(new GenerateKeyRequest
             {
@@ -43,12 +51,19 @@ namespace DIDComm.Messaging.Tests
                 Seed = ByteString.CopyFrom(StringToByteArray(seed))
             }); ;
 
+            byte[] x = System.Convert.FromBase64String(response.Key.X);
+            byte[] y = System.Convert.FromBase64String(response.Key.Y);
+            byte[] pk = new byte[x.Length + y.Length];
+
+            System.Buffer.BlockCopy(x, 0, pk, 0, x.Length);
+            System.Buffer.BlockCopy(y, 0, pk, x.Length, y.Length);
+
             response.Should().NotBeNull();
             response.Key.Should().NotBeNull();
-            response.Key.KeyType.Should().Be(keyType);
-            response.Key.PublicKey.Should().NotBeNull().And.HaveCount(32);
-            response.Key.SecretKey.Should().NotBeNull().And.HaveCount(32);
-            publicKey.Should().Be(Multibase.Base58.Encode(response.Key.PublicKey.ToByteArray()));
+            response.Key.Crv.Should().Be(keyType);
+            pk.Should().NotBeNull().And.HaveCount(32);
+            System.Convert.FromBase64String(response.Key.D).Should().NotBeNull().And.HaveCount(32);
+            publicKey.Should().Be(Multibase.Base58.Encode(pk));
         }
 
         public static byte[] StringToByteArray(string hex)
