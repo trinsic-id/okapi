4802b7e1009a2bb7791a4da92fa0f53499ad7da2
Tomislav Markovski <tomislav@trinsic.id>
2021-05-06 13:04:31 -0400

    Update wasm

--- wasm/packages/didcomm-proto/src/proto/api_pb.d.ts
+++ wasm/packages/didcomm-proto/src/proto/api_pb.d.ts
@@ -442,8 +442,15 @@ export namespace ResolveRequest {
 }
 
 export class ResolveResponse extends jspb.Message {
-  getDiddoc(): string;
-  setDiddoc(value: string): void;
+  hasDidDocument(): boolean;
+  clearDidDocument(): void;
+  getDidDocument(): google_protobuf_struct_pb.Struct | undefined;
+  setDidDocument(value?: google_protobuf_struct_pb.Struct): void;
+
+  clearKeysList(): void;
+  getKeysList(): Array<JsonWebKey>;
+  setKeysList(value: Array<JsonWebKey>): void;
+  addKeys(value?: JsonWebKey, index?: number): JsonWebKey;
 
   serializeBinary(): Uint8Array;
   toObject(includeInstance?: boolean): ResolveResponse.AsObject;
@@ -457,7 +464,136 @@ export class ResolveResponse extends jspb.Message {
 
 export namespace ResolveResponse {
   export type AsObject = {
-    diddoc: string,
+    didDocument?: google_protobuf_struct_pb.Struct.AsObject,
+    keysList: Array<JsonWebKey.AsObject>,
+  }
+}
+
+export class ResolveKeyRequest extends jspb.Message {
+  getDidUri(): string;
+  setDidUri(value: string): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ResolveKeyRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: ResolveKeyRequest): ResolveKeyRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ResolveKeyRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ResolveKeyRequest;
+  static deserializeBinaryFromReader(message: ResolveKeyRequest, reader: jspb.BinaryReader): ResolveKeyRequest;
+}
+
+export namespace ResolveKeyRequest {
+  export type AsObject = {
+    didUri: string,
+  }
+}
+
+export class ResolveKeyResponse extends jspb.Message {
+  hasKey(): boolean;
+  clearKey(): void;
+  getKey(): JsonWebKey | undefined;
+  setKey(value?: JsonWebKey): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): ResolveKeyResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: ResolveKeyResponse): ResolveKeyResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: ResolveKeyResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): ResolveKeyResponse;
+  static deserializeBinaryFromReader(message: ResolveKeyResponse, reader: jspb.BinaryReader): ResolveKeyResponse;
+}
+
+export namespace ResolveKeyResponse {
+  export type AsObject = {
+    key?: JsonWebKey.AsObject,
+  }
+}
+
+export class CreateProofRequest extends jspb.Message {
+  hasDocument(): boolean;
+  clearDocument(): void;
+  getDocument(): google_protobuf_struct_pb.Struct | undefined;
+  setDocument(value?: google_protobuf_struct_pb.Struct): void;
+
+  hasKey(): boolean;
+  clearKey(): void;
+  getKey(): JsonWebKey | undefined;
+  setKey(value?: JsonWebKey): void;
+
+  getSuite(): LdSuiteMap[keyof LdSuiteMap];
+  setSuite(value: LdSuiteMap[keyof LdSuiteMap]): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): CreateProofRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: CreateProofRequest): CreateProofRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: CreateProofRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): CreateProofRequest;
+  static deserializeBinaryFromReader(message: CreateProofRequest, reader: jspb.BinaryReader): CreateProofRequest;
+}
+
+export namespace CreateProofRequest {
+  export type AsObject = {
+    document?: google_protobuf_struct_pb.Struct.AsObject,
+    key?: JsonWebKey.AsObject,
+    suite: LdSuiteMap[keyof LdSuiteMap],
+  }
+}
+
+export class CreateProofResponse extends jspb.Message {
+  hasSignedDocument(): boolean;
+  clearSignedDocument(): void;
+  getSignedDocument(): google_protobuf_struct_pb.Struct | undefined;
+  setSignedDocument(value?: google_protobuf_struct_pb.Struct): void;
+
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): CreateProofResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: CreateProofResponse): CreateProofResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: CreateProofResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): CreateProofResponse;
+  static deserializeBinaryFromReader(message: CreateProofResponse, reader: jspb.BinaryReader): CreateProofResponse;
+}
+
+export namespace CreateProofResponse {
+  export type AsObject = {
+    signedDocument?: google_protobuf_struct_pb.Struct.AsObject,
+  }
+}
+
+export class VerifyProofRequest extends jspb.Message {
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): VerifyProofRequest.AsObject;
+  static toObject(includeInstance: boolean, msg: VerifyProofRequest): VerifyProofRequest.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: VerifyProofRequest, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): VerifyProofRequest;
+  static deserializeBinaryFromReader(message: VerifyProofRequest, reader: jspb.BinaryReader): VerifyProofRequest;
+}
+
+export namespace VerifyProofRequest {
+  export type AsObject = {
+  }
+}
+
+export class VerifyProofResponse extends jspb.Message {
+  serializeBinary(): Uint8Array;
+  toObject(includeInstance?: boolean): VerifyProofResponse.AsObject;
+  static toObject(includeInstance: boolean, msg: VerifyProofResponse): VerifyProofResponse.AsObject;
+  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
+  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
+  static serializeBinaryToWriter(message: VerifyProofResponse, writer: jspb.BinaryWriter): void;
+  static deserializeBinary(bytes: Uint8Array): VerifyProofResponse;
+  static deserializeBinaryFromReader(message: VerifyProofResponse, reader: jspb.BinaryReader): VerifyProofResponse;
+}
+
+export namespace VerifyProofResponse {
+  export type AsObject = {
   }
 }
 
@@ -470,4 +606,10 @@ export interface KeyTypeMap {
 }
 
 export const KeyType: KeyTypeMap;
+
+export interface LdSuiteMap {
+  JCSED25519SIGNATURE2020: 0;
+}
+
+export const LdSuite: LdSuiteMap;
 
