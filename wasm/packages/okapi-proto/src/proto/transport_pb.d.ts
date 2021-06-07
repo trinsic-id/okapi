c12568c3f1503c0fa0aa13f3bbd022c0b08b9b89
Tomislav Markovski <tomislav@trinsic.id>
2021-06-07 09:05:14 -0400

    Remove grpc dep

--- wasm/packages/okapi-proto/src/proto/transport_pb.d.ts
+++ wasm/packages/okapi-proto/src/proto/transport_pb.d.ts
@@ -283,19 +283,3 @@ export namespace CoreMessage {
   }
 }
 
-export class NoOp extends jspb.Message {
-  serializeBinary(): Uint8Array;
-  toObject(includeInstance?: boolean): NoOp.AsObject;
-  static toObject(includeInstance: boolean, msg: NoOp): NoOp.AsObject;
-  static extensions: {[key: number]: jspb.ExtensionFieldInfo<jspb.Message>};
-  static extensionsBinary: {[key: number]: jspb.ExtensionFieldBinaryInfo<jspb.Message>};
-  static serializeBinaryToWriter(message: NoOp, writer: jspb.BinaryWriter): void;
-  static deserializeBinary(bytes: Uint8Array): NoOp;
-  static deserializeBinaryFromReader(message: NoOp, reader: jspb.BinaryReader): NoOp;
-}
-
-export namespace NoOp {
-  export type AsObject = {
-  }
-}
-
