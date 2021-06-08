90a75cb54451c9c8836c9f51f2a2bc6bf4fcd943
Tomislav Markovski <tomislav@trinsic.id>
2021-06-08 14:13:43 -0400

    Tag 19

--- wasm/examples/webpack-sample/webpack.config.js
+++ wasm/examples/webpack-sample/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './index.js',
   output: {
-    path: path.resolve(__dirname, '.'),
+    path: path.resolve(__dirname, ''),
     filename: 'bundle.js',
   },
 };
