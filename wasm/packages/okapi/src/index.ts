63390ca4cbdc1eabe35eaacc3d262780e7865212
Tomislav Markovski <tmarkovski@gmail.com>
2021-06-08 08:05:56 -0400

    Make okapi wasm api async using promises (#45)
    * Make okapi wasm api async using promises
    
    * Add missing web api
    
    Co-authored-by: Tomislav Markovski <tomislav@trinsic.id>

--- wasm/packages/okapi/src/index.ts
+++ wasm/packages/okapi/src/index.ts
@@ -3,54 +3,40 @@ import * as proto from "@trinsic/okapi-proto";
 export * from "@trinsic/okapi-proto";
 
 export class DIDKey {
-  static generate(
-    request: proto.GenerateKeyRequest
-  ): proto.GenerateKeyResponse {
-    return proto.GenerateKeyResponse.deserializeBinary(
-      native.didkey_generate(request.serializeBinary())
+  static generate(request: proto.GenerateKeyRequest): Promise<proto.GenerateKeyResponse> {
+    return Promise.resolve(
+      proto.GenerateKeyResponse.deserializeBinary(native.didkey_generate(request.serializeBinary()))
     );
   }
-  static resolve(request: proto.ResolveRequest): proto.ResolveResponse {
-    return proto.ResolveResponse.deserializeBinary(
-      native.didkey_resolve(request.serializeBinary())
-    );
+  static resolve(request: proto.ResolveRequest): Promise<proto.ResolveResponse> {
+    return Promise.resolve(proto.ResolveResponse.deserializeBinary(native.didkey_resolve(request.serializeBinary())));
   }
 }
 
 export class DIDComm {
-  static pack(request: proto.PackRequest): proto.PackResponse {
-    return proto.PackResponse.deserializeBinary(
-      native.didcomm_pack(request.serializeBinary())
-    );
+  static pack(request: proto.PackRequest): Promise<proto.PackResponse> {
+    return Promise.resolve(proto.PackResponse.deserializeBinary(native.didcomm_pack(request.serializeBinary())));
   }
-  static unpack(request: proto.UnpackRequest): proto.UnpackResponse {
-    return proto.UnpackResponse.deserializeBinary(
-      native.didcomm_unpack(request.serializeBinary())
-    );
+  static unpack(request: proto.UnpackRequest): Promise<proto.UnpackResponse> {
+    return Promise.resolve(proto.UnpackResponse.deserializeBinary(native.didcomm_unpack(request.serializeBinary())));
   }
-  static sign(request: proto.SignRequest): proto.SignResponse {
-    return proto.SignResponse.deserializeBinary(
-      native.didcomm_sign(request.serializeBinary())
-    );
+  static sign(request: proto.SignRequest): Promise<proto.SignResponse> {
+    return Promise.resolve(proto.SignResponse.deserializeBinary(native.didcomm_sign(request.serializeBinary())));
   }
-  static verify(request: proto.VerifyRequest): proto.VerifyResponse {
-    return proto.VerifyResponse.deserializeBinary(
-      native.didcomm_verify(request.serializeBinary())
-    );
+  static verify(request: proto.VerifyRequest): Promise<proto.VerifyResponse> {
+    return Promise.resolve(proto.VerifyResponse.deserializeBinary(native.didcomm_verify(request.serializeBinary())));
   }
 }
 
 export class LdProofs {
-  static generate(
-    request: proto.CreateProofRequest
-  ): proto.CreateProofResponse {
-    return proto.CreateProofResponse.deserializeBinary(
-      native.ldproofs_create_proof(request.serializeBinary())
+  static generate(request: proto.CreateProofRequest): Promise<proto.CreateProofResponse> {
+    return Promise.resolve(
+      proto.CreateProofResponse.deserializeBinary(native.ldproofs_create_proof(request.serializeBinary()))
     );
   }
-  static convert(request: proto.VerifyProofRequest): proto.VerifyProofResponse {
-    return proto.VerifyProofResponse.deserializeBinary(
-      native.ldproofs_verify_proof(request.serializeBinary())
+  static convert(request: proto.VerifyProofRequest): Promise<proto.VerifyProofResponse> {
+    return Promise.resolve(
+      proto.VerifyProofResponse.deserializeBinary(native.ldproofs_verify_proof(request.serializeBinary()))
     );
   }
 }
