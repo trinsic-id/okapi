75e888ebc6d690644e2c2caaff9d69bdf4ac0dbd
fundthmcalculus <polygonguru@gmail.com>
2021-06-21 16:21:48 -0400

    Force exit code 0

--- dotnet/Test.ps1
+++ dotnet/Test.ps1
@@ -1,4 +1,6 @@
 dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=cobertura /p:ExcludeByFile=\"**/obj/**/*.cs\"
-if ($LASTEXITCODE -lt 0 -Or $LASTEXITCODE -gt 1) {
-    $host.SetShouldExit($LASTEXITCODE)
+if ($LASTEXITCODE -ge 0 -Or $LASTEXITCODE -le 1) {
+    exit 0
+} else {
+    exit $LASTEXITCODE
 }
