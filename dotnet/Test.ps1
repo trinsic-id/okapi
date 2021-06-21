4f369b174b6ef5627899da0b3094f1c96a96a834
fundthmcalculus <polygonguru@gmail.com>
2021-06-21 12:57:51 -0400

    no message

--- dotnet/Test.ps1
+++ dotnet/Test.ps1
@@ -1,6 +1,4 @@
-$exitCode = dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=cobertura /p:ExcludeByFile=\"**/obj/**/*.cs\"
-if ($exitCode -ge 0 -And $exitCode -le 1) {
-    $host.SetShouldExit(0)
-} else {
-    $host.SetShouldExit($exitCode)
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=cobertura /p:ExcludeByFile=\"**/obj/**/*.cs\"
+if ($LASTEXITCODE -lt 0 -Or $LASTEXITCODE -gt 1) {
+    $host.SetShouldExit($LASTEXITCODE)
 }
