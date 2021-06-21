6ba966923fd8139f6e4753cdd34fd66e1da05a9c
Scott Phillips <polygonguru@gmail.com>
2021-06-21 16:38:02 -0400

    Dotnet code coverage (#55)

---
+++ dotnet/Test.ps1
@@ -0,0 +1,6 @@
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=cobertura /p:ExcludeByFile=\"**/obj/**/*.cs\"
+if ($LASTEXITCODE -ge 0 -Or $LASTEXITCODE -le 1) {
+    exit 0
+} else {
+    exit $LASTEXITCODE
+}
