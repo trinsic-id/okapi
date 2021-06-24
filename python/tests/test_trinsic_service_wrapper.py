2af7a7abf15795114c28eef61e35b569f075e15a
fundthmcalculus <polygonguru@gmail.com>
2021-06-24 08:43:03 -0400

    no message

--- python/tests/test_trinsic_service_wrapper.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-
-class TrinsicServiceTests(unittest.TestCase):
-    def test_generate_capability_invocation_proof_with_jcs(self):
-        # TODO - Implement tests.
-        self.assertEqual(True, False)
