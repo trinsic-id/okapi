159510a7926cc2086550f575239529f1ad4bd3db
fundthmcalculus <polygonguru@gmail.com>
2021-06-24 11:06:14 -0400

    Post comments on coverage report

--- dotnet/Test.ps1
+++ dotnet/Test.ps1
@@ -1,4 +1,4 @@
-dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=cobertura /p:ExcludeByFile=\"**/obj/**/*.cs\"
+dotnet test /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=\"cobertura,opencover\" /p:ExcludeByFile=\"**/obj/**/*.cs\"
 if ($LASTEXITCODE -ge 0 -Or $LASTEXITCODE -le 1) {
     exit 0
 } else {
