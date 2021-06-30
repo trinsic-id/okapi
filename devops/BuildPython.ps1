5882b7f518c2f955e1aef6d46ae0977b27e4dedf
Scott Phillips <polygonguru@gmail.com>
2021-06-30 17:12:18 -0400

    Build script

--- devops/BuildPython.ps1
+++ devops/BuildPython.ps1
@@ -1,20 +1,58 @@
+param
+(
+    [string]$GitTag
+)
 
-function Activate-Venv() {
-	param ([string]$path)
-	
+. "./VersionParse.ps1"
+
+function Activate-Venv {
+	try {
+		if ($IsWindows) {
+			 .\venv\Scripts\activate
+		} else {
+			source env/bin/activate
+		}
+	} finally {
+		if ($IsWindows) {
+			Write-Output (where python)
+		} else {
+			Write-Output (which python)
+		}
+	}
 }
-# TODO Activate venv if exists
+function Install-Requirements {
+	python -m pip install --upgrade pip
+	python -m pip install -r requirements.txt
+	python -m pip install pytest pytest-cov build
+}
+function Run-Tests {
+	python -m pytest --cache-clear ./tests --junitxml=pytest_junit.xml --cov=.
+}
+function Build-Package {
+	try {
+		# Get release version from tag and set the setup.cfg file
+		$pythonPackageVersion = Get-PythonVersion($GitTag)
+		$setupConfig = Get-Content ./setup.cfg
+		for ($ij = 0; $ij -lt $setupConfig.Length; $ij++) {
+			if ($setupConfig[$ij].StartsWith("version")) {
+				$setupConfig[$ij] = "version = " + $pythonPackageVersion
+			}
+		}
+		Set-Content -path ./setup.cfg -value $setupConfig
+	} catch {
+	}finally {
+		python -m build --sdist --wheel --outdir dist/ .
+	}
+}
+
 # Setup
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
-python -m pip install pytest pytest-cov
-
-# Run tests
-python -m pytest --cache-clear ./tests --junitxml=pytest_junit.xml --cov=.
-
-# TODO: Activate venv if it exists
-python -m pip install build
-# TODO: Get release version from tag and set the setup.cfg file
-
-# Build package
-python -m build --sdist --wheel --outdir dist/ .
+$InvocationPath = (Get-Item .).FullName
+cd ../python
+# Do the things
+Activate-Venv
+Install-Requirements
+Run-Tests
+Build-Package
+# Leave the venv
+deactivate
+Set-Location $InvocationPath
