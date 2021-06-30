2d3515e35f89b7ae97de49d7b9bd5034ff8b0b99
fundthmcalculus <polygonguru@gmail.com>
2021-06-30 09:23:33 -0400

    initial setup

---
+++ python/package.ps1
@@ -0,0 +1,6 @@
+﻿# TODO: Activate venv if it exists
+python -m pip install build
+# TODO: Get release version from tag and set the setup.cfg file
+
+# Build package
+python -m build --sdist --wheel --outdir dist/ .
