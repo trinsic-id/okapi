c3cd7c3f46a080549e79eb7365cedf18eea7189d
Scott Phillips <polygonguru@gmail.com>
2021-07-01 09:44:27 -0400

    SDK-44 Python build script (#83)

---
+++ devops/BuildPython.ps1
@@ -0,0 +1,59 @@
+param
+(
+    [string]$GitTag
+)
+
+. "./VersionParse.ps1"
+
+function Activate-Venv {
+	try {
+		if ($IsWindows) {
+			 .\venv\Scripts\activate
+		} else {
+			source env/bin/activate
+		}
+	} finally {
+		if ($IsWindows) {
+			Write-Output (where python)
+		} else {
+			Write-Output (which python)
+		}
+	}
+}
+function Install-Requirements {
+	python -m pip install --upgrade pip
+	python -m pip install -r requirements.txt
+	python -m pip install pytest pytest-cov build
+}
+function Run-Tests {
+	python -m pytest --cache-clear ./tests --junitxml=pytest_junit.xml --cov=.
+}
+function Build-Package {
+	try {
+		# Get release version from tag and set the setup.cfg file
+		$pythonPackageVersion = Get-PythonVersion($GitTag)
+		$setupConfig = Get-Content ./setup.cfg
+		for ($ij = 0; $ij -lt $setupConfig.Length; $ij++) {
+			if ($setupConfig[$ij].StartsWith("version")) {
+				$setupConfig[$ij] = "version = " + $pythonPackageVersion
+			}
+		}
+		Set-Content -path ./setup.cfg -value $setupConfig
+	} catch {
+	}finally {
+		python -m build --sdist --wheel --outdir dist/ .
+	}
+}
+
+# Setup
+$InvocationPath = (Get-Item .).FullName
+cd ../python
+Copy-Item "../libs/*" -Destination "./okapi/libs" -Recurse
+# Do the things
+Activate-Venv
+Install-Requirements
+Run-Tests
+Build-Package
+# Leave the venv
+deactivate
+Set-Location $InvocationPath
