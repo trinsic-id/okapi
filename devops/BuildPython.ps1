33d43b81e51b6b83e42466f38641ec65408cb9cb
Scott Phillips <polygonguru@gmail.com>
2021-07-02 17:14:13 -0400

    Package build updates (#93)

--- devops/BuildPython.ps1
+++ devops/BuildPython.ps1
@@ -1,53 +1,62 @@
 param
 (
-    [string]$GitTag
+    [string]$GitTag = '',
+    [string]$PackageVersion = ''
 )
 
 . "$PSScriptRoot/VersionParse.ps1"
 
 function Activate-Venv {
-	try {
-		if ($IsWindows) {
-			 .\venv\Scripts\activate
-		} else {
-			./venv/bin/activate
-		}
-	} catch {
-	} finally {
-		Write-Output (Where-Object python)
-	}
+    try {
+        if ($IsWindows) {
+             .\venv\Scripts\activate
+        } else {
+            ./venv/bin/activate
+        }
+    } catch {
+    } finally {
+        Write-Output (Where-Object python)
+    }
 }
 function Install-Requirements {
-	python -m pip install --upgrade pip
-	python -m pip install -r requirements.txt
-	python -m pip install pytest pytest-cov build
+    python -m pip install --upgrade pip
+    python -m pip install -r requirements.txt
+    python -m pip install pytest pytest-cov build
 
-	python ./okapi/generate_proto_files.py
+    python ./okapi/generate_proto_files.py
 }
 function Run-Tests {
-	python -m pytest --cache-clear ./tests --junitxml=pytest_junit.xml --cov=.
+    python -m pytest --cache-clear ./tests --junitxml=pytest_junit.xml --cov=.
+}
+function Set-Python-Version {
+    param ([string]$pythonPackageVersion = '')
+    $setupConfig = Get-Content ./setup.cfg
+    for ($ij = 0; $ij -lt $setupConfig.Length; $ij++) {
+        if ($setupConfig[$ij].StartsWith("version")) {
+            $setupConfig[$ij] = "version = " + $pythonPackageVersion
+        }
+    }
+    Set-Content -path ./setup.cfg -value $setupConfig
 }
 function Build-Package {
-	try {
-		# Get release version from tag and set the setup.cfg file
-		$pythonPackageVersion = Get-PythonVersion($GitTag)
-		$setupConfig = Get-Content ./setup.cfg
-		for ($ij = 0; $ij -lt $setupConfig.Length; $ij++) {
-			if ($setupConfig[$ij].StartsWith("version")) {
-				$setupConfig[$ij] = "version = " + $pythonPackageVersion
-			}
-		}
-		Set-Content -path ./setup.cfg -value $setupConfig
-	} catch {
-	}finally {
-		python -m build --sdist --wheel --outdir dist/ .
-	}
+    try {
+        # Get release version from tag and set the setup.cfg file
+        $pythonPackageVersion = Get-PythonVersion($GitTag)
+        Set-Python-Version($pythonPackageVersion)
+    } catch {
+        if ($PackageVersion -ne '') {
+            Set-Python-Version($PackageVersion)
+        }
+    }finally {
+        
+        python -m build --sdist --wheel --outdir dist/ .
+    }
 }
 
 # Setup
 $InvocationPath = (Get-Item .).FullName
 Set-Location "$PSScriptRoot/../python"
-Copy-Item "../libs/*" -Destination "./okapi/libs" -Recurse
+Copy-Item "../libs/*" -Destination "./okapi/libs" -Recurse -Force
 # Do the things
 Activate-Venv
 Install-Requirements
@@ -55,7 +64,7 @@ Run-Tests
 Build-Package
 # Leave the venv
 try {
-	deactivate
+    deactivate
 } catch {
 }
 Set-Location $InvocationPath
