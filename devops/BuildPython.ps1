2d0ec479786ca65d6f75fb1c20e1d3b3d52137f9
Scott Phillips <polygonguru@gmail.com>
2021-07-02 17:43:12 -0400

    no message

--- devops/BuildPython.ps1
+++ devops/BuildPython.ps1
@@ -1,7 +1,7 @@
 param
 (
-    [string]$GitTag = ' ',
-    [string]$PackageVersion = ' '
+    [AllowNull()][string]$GitTag = '',
+    [AllowNull()][string]$PackageVersion = ''
 )
 
 . "$PSScriptRoot/VersionParse.ps1"
