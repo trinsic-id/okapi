f4b76ed9f099d625580035e801649cb99054c482
Scott Phillips <polygonguru@gmail.com>
2021-07-09 12:47:19 -0400

    CBindgen cargo build (#99)

---
+++ native/src/ffi/utils.rs
@@ -0,0 +1,27 @@
+#[macro_use]
+
+use ffi_support::{ByteBuffer, ExternError};
+
+// TODO: Cargo build doesn't expand these correctly with cbindgen, manually expanded below.
+// define_string_destructor!(didcomm_string_free2);
+// define_bytebuffer_destructor!(didcomm_byte_buffer_free2);
+
+#[no_mangle]
+pub extern "C" fn didcomm_byte_buffer_free(v: ByteBuffer) {
+    // Note: This should never happen, but in the case of a bug aborting
+    // here is better than the badness that happens if we unwind across
+    // the FFI boundary.
+    ffi_support::abort_on_panic::with_abort_on_panic(|| v.destroy())
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn didcomm_string_free(s: *mut std::os::raw::c_char) {
+    // Note: This should never happen, but in the case of a bug aborting
+    // here is better than the badness that happens if we unwind across
+    // the FFI boundary.
+    ffi_support::abort_on_panic::with_abort_on_panic(|| {
+        if !s.is_null() {
+            ffi_support::destroy_c_string(s)
+        }
+    });
+}
