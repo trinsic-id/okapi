8223e5fc34e25e45dfedb4dac95d500cf19d2e2b
Tomislav Markovski <tomislav@trinsic.id>
2021-07-29 15:41:21 -0400

    Bump version

--- wasm/examples/webpack-sample/index.js
+++ wasm/examples/webpack-sample/index.js
@@ -1,16 +1,13 @@
 import { DIDKey, GenerateKeyRequest, KeyType } from "@trinsic/okapi";
 async function generateKey() {
+  var request = new GenerateKeyRequest().setKeyType(KeyType.ED25519);
 
-    var request = new GenerateKeyRequest();
-    request.setKeyType(KeyType.ED25519);
+  var response = await DIDKey.generate(request);
 
-    var response = await DIDKey.generate(request);
+  var didDocument = response.getDidDocument().toJavaScript();
+  console.log(didDocument);
 
-    var didDocument = response.getDidDocument().toJavaScript();
-    console.log(didDocument);
-    console.log(Uint8Array.from([16, 3]));
-
-    document.getElementById("did-document").innerText = JSON.stringify(didDocument, null, "\t");
+  document.getElementById("did-document").innerText = JSON.stringify(didDocument, null, "\t");
 }
 
 generateKey();
