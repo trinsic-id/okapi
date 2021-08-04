ef8ceb56530261244b8fe750a2964e1280b0987c
Scott Phillips <polygonguru@gmail.com>
2021-08-04 15:52:33 -0400

    no message

--- go/buildGoProto.ps1
+++ go/buildGoProto.ps1
@@ -1,5 +1,3 @@
 protoc --proto_path=../proto --go_out=okapi --go_opt=paths=source_relative ../proto/keys.proto ../proto/proofs.proto ../proto/transport.proto ../proto/examples.proto ../proto/pbmse/pbmse.proto
-# TODO - Copy pbmse up one level and delete folder
 Copy-Item -Path "./okapi/pbmse/pbmse.pb.go" "./okapi/pbmse.pb.go"
 Remove-Item -Path "./okapi/pbmse" -Force -Recurse
- #>
