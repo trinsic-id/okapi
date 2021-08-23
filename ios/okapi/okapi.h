34998604f162868e5b00e52ad42595b5926a62f7
Scott Phillips <polygonguru@gmail.com>
2021-08-23 08:28:42 -0400

    initial build of pod

---
+++ ios/okapi/okapi.h
@@ -0,0 +1,8 @@
+//! Project version number for okapi.
+FOUNDATION_EXPORT double okapiVersionNumber;
+
+//! Project version string for okapi.
+FOUNDATION_EXPORT const unsigned char okapiVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <okapi/PublicHeader.h>
+
