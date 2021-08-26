73d1db3e7b7d0740f5225ca3fccdee019f499fdb
Scott Phillips <polygonguru@gmail.com>
2021-08-26 09:56:16 -0400

    updates

---
+++ ios/build/okapi.build/Debug-iphoneos/okapi.build/DerivedSources/okapi_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char okapiVersionString[];
+ extern const double okapiVersionNumber;
+
+ const unsigned char okapiVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:okapi  PROJECT:okapi-1" "\n";
+ const double okapiVersionNumber __attribute__ ((used)) = (double)1.;
