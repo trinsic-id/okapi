d845a59099de443541967abc50dd2bd995983a52
Scott Phillips <polygonguru@gmail.com>
2021-09-08 10:27:23 -0400

    cargo-lipo doesn't work on macos

--- devops/BuildLibraries.ps1
+++ devops/BuildLibraries.ps1
@@ -46,16 +46,12 @@ try {
             break
         }
         MacOS {
-            cargo install cargo-lipo
             rustup target add x86_64-apple-darwin aarch64-apple-darwin
-            # cargo build --release --target x86_64-apple-darwin
-            # cargo build --release --target aarch64-apple-darwin
-            cargo lipo --release
-            Copy-Item -Path "./target/universal/release/libokapi.a" -Destination $TargetOutput
-            Copy-Item -Path "./target/universal/release/libokapi.dylib" -Destination $TargetOutput
+            cargo build --release --target x86_64-apple-darwin
+            cargo build --release --target aarch64-apple-darwin
             # Create the fat binaries.
-            # lipo -create .\target\x86_64-apple-darwin\release\libokapi.a .\target\aarch64-apple-darwin\release\libokapi.a -output $TargetOutput\libokapi.a
-            # lipo -create .\target\x86_64-apple-darwin\release\libokapi.dylib .\target\aarch64-apple-darwin\release\libokapi.dylib -output $TargetOutput\libokapi.dylib
+            lipo -create ".\target\x86_64-apple-darwin\release\libokapi.a" ".\target\aarch64-apple-darwin\release\libokapi.a" -output "$TargetOutput\libokapi.a"
+            lipo -create ".\target\x86_64-apple-darwin\release\libokapi.dylib" ".\target\aarch64-apple-darwin\release\libokapi.dylib" -output "$TargetOutput\libokapi.dylib"
             break
         }
         iOS {
