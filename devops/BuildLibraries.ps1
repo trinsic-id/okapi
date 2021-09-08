1f86e3f6b2e0185c59bd5fd6f050639932b6ba06
Scott Phillips <polygonguru@gmail.com>
2021-09-08 14:27:17 -0400

    put the simulators in a fat binary, real iOS in it's own due to a conflict between ARM64 M1 sim and ARM64 device

--- devops/BuildLibraries.ps1
+++ devops/BuildLibraries.ps1
@@ -73,7 +73,8 @@ try {
             cargo build --release --target aarch64-apple-ios
 
             # Create the fat binaries, cargo-lipo doesn't support ios sim aarch64
-            lipo -create "./target/x86_64-apple-ios/release/libokapi.a" "./target/aarch64-apple-ios/release/libokapi.a" "./target/aarch64-apple-ios-sim/release/libokapi.a" -output "$TargetOutput/libokapi.a"
+            lipo -create "./target/x86_64-apple-ios/release/libokapi.a" "./target/aarch64-apple-ios-sim/release/libokapi.a" -output "$TargetOutput/libokapi_simulator.a"
+            Copy-Item -Path "./target/aarch64-apple-ios/release/libokapi.a" -Destination "$TargetOutput/libokapi.a"
             break
         }
         Android {
