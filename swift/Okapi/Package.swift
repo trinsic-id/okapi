bbc2377bad61da8ba39960d4511d459d965d5a00
Scott Phillips <polygonguru@gmail.com>
2021-09-09 11:09:22 -0400

    Swift support (#158)
    * initial round of swift support
    
    * more ignore
    
    * Updates, and remove the old ios work
    
    * more swift updates

---
+++ swift/Okapi/Package.swift
@@ -0,0 +1,50 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+import Foundation
+
+var filePath = URL(fileURLWithPath: #filePath)
+        .deletingLastPathComponent()
+        .appendingPathComponent("Sources")
+        .appendingPathComponent("OkapiObjectiveC");
+let linkFlag: String = "-L\(filePath.relativePath)";
+
+let package = Package(
+        name: "Okapi",
+        platforms: [
+            .iOS(.v10), .macOS(.v11)
+        ],
+        products: [
+            // Products define the executables and libraries a package produces, and make them visible to other packages.
+            .library(
+                    name: "TrinsicOkapi",
+                    targets: ["OkapiSwift", "OkapiObjectiveC"]),
+        ],
+        dependencies: [
+            // Dependencies declare other packages that this package depends on.
+            .package(name: "Base58Swift", url: "https://github.com/keefertaylor/Base58Swift.git", from: "2.1.0"),
+            .package(name: "SwiftProtobuf", url: "https://github.com/apple/swift-protobuf.git", from: "1.17.0"),
+        ],
+        targets: [
+            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+            // Targets can depend on other targets in this package, and on products in packages this package depends on.
+            .target(
+                    name: "OkapiObjectiveC",
+                    dependencies: [],
+                    exclude: ["libokapi.a", "libokapi_simulator.a", "libokapi_ios.a"],
+                    linkerSettings: [
+                        LinkerSetting.linkedLibrary("okapi", .when(platforms: [.macOS])),
+                        LinkerSetting.linkedLibrary("okapi_ios", .when(platforms: [.iOS])),
+                        // TODO - Support the simulator
+                        LinkerSetting.unsafeFlags([linkFlag], .when(platforms: [.macOS, .iOS]))
+                    ]
+            ),
+            .target(
+                    name: "OkapiSwift",
+                    dependencies: ["OkapiObjectiveC", "SwiftProtobuf"]),
+            .testTarget(
+                    name: "OkapiTests",
+                    dependencies: ["OkapiSwift", "Base58Swift"]),
+        ]
+)
