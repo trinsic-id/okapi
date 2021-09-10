c44b069bbff8166c5c9903c4864890eac8156448
Scott Phillips <polygonguru@gmail.com>
2021-09-10 16:56:10 -0400

    Update swift correctly

--- swift/Okapi/buildProto.ps1
+++
@@ -1,9 +0,0 @@
-Set-Location $PSScriptRoot
-protoc --proto_path=../../proto `
-       --swift_out=./Sources/OkapiSwift/proto `
-       --swift_opt=Visibility=Public `
-       ../../proto/examples.proto `
-       ../../proto/keys.proto `
-       ../../proto/proofs.proto `
-       ../../proto/transport.proto `
-       ../../proto/pbmse/pbmse.proto
