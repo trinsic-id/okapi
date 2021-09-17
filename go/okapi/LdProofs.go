c65ce4f91bcc67d035ef2d74190a9a1f22347c8d
Scott Phillips <polygonguru@gmail.com>
2021-09-17 12:59:02 -0400

    Error handling in golang okapi (#168)

--- go/okapi/LdProofs.go
+++ go/okapi/LdProofs.go
@@ -4,22 +4,18 @@ import "C"
 
 type LdProofs struct {}
 type ILdProofs interface {
-	CreateProof(request *CreateProofRequest) (CreateProofResponse, error)
-	VerifyProof(request *VerifyProofRequest) (VerifyProofResponse, error)
+	CreateProof(request *CreateProofRequest) (*CreateProofResponse, error)
+	VerifyProof(request *VerifyProofRequest) (*VerifyProofResponse, error)
 }
 
-func (l LdProofs) CreateProof(request *CreateProofRequest) (CreateProofResponse, error) {
+func (l LdProofs) CreateProof(request *CreateProofRequest) (*CreateProofResponse, error) {
 	response := CreateProofResponse{}
-	requestBuffer, responseBuffer, err := createBuffersFromMessage(request)
-	code := ldproofs_create_proof(requestBuffer, &responseBuffer, &err)
-	unmarshalResponse(responseBuffer, &response, requestBuffer)
-	return response, createError("ldproofs_create_proof", code, err)
+	err := callOkapiNative(request, &response, ldproofsCreateProof)
+	return &response, err
 }
 
-func (l LdProofs) VerifyProof(request *VerifyProofRequest) (VerifyProofResponse, error) {
+func (l LdProofs) VerifyProof(request *VerifyProofRequest) (*VerifyProofResponse, error) {
 	response := VerifyProofResponse{}
-	requestBuffer, responseBuffer, err := createBuffersFromMessage(request)
-	code := ldproofs_verify_proof(requestBuffer, &responseBuffer, &err)
-	unmarshalResponse(responseBuffer, &response, requestBuffer)
-	return response, createError("ldproofs_verify_proof", code, err)
+	err := callOkapiNative(request, &response, ldproofsVerifyProof)
+	return &response, err
 }
