b2e8abee4046c18f0d2355d70fd70202292e9fee
Tomislav Markovski <tmarkovski@gmail.com>
2021-09-23 09:26:52 -0400

    Remove net sample. Update example proto (#178)
    * Fix net example
    
    * Remove dotnet sample
    
    * Comment scripts

--- dotnet/Test.ps1
+++ dotnet/Test.ps1
@@ -1,3 +1,5 @@
+# Runs the tests and collects coverage results
+
 dotnet test --% /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=\"cobertura,json\" /p:ExcludeByFile=\"**/obj/**/*.cs\"
 if ($LASTEXITCODE -ge 0 -Or $LASTEXITCODE -le 1) {
     exit 0
