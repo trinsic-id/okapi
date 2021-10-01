1434d2851c365d802651794bf478dfaa3a4b0702
Scott Phillips <polygonguru@gmail.com>
2021-10-01 11:14:29 -0400

    no message

--- devops/BuildSwift.ps1
+++ devops/BuildSwift.ps1
@@ -26,7 +26,7 @@ $dest = "./Okapi/Sources/OkapiObjectiveC"
 Get-ChildItem $source -Recurse | `
     Where-Object { $_.PSIsContainer -eq $False -and $_.Extension -eq ".a" } | `
     ForEach-Object {Copy-Item -Path $_.Fullname -Destination $dest -Force} # Do the things
-Copy-Item -Path "$source/C_header/okapi.h" -Destination "$dest/include" -Force
+Copy-Item -Path "$source/../C_header/okapi.h" -Destination "$dest/include" -Force
 Install-Requirements
 if (!$RequirementsOnly) {
     Run-Tests
