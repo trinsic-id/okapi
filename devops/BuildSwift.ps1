18cadb51c4f104d8998ca6f0012e194861b78118
Scott Phillips <polygonguru@gmail.com>
2021-10-01 11:04:41 -0400

    package rename

--- devops/BuildSwift.ps1
+++ devops/BuildSwift.ps1
@@ -24,7 +24,7 @@ Set-Location "$PSScriptRoot/../swift"
 $source = "../libs"
 $dest = "./Okapi/Sources/OkapiObjectiveC"
 Get-ChildItem $source -Recurse | `
-    Where-Object { $_.PSIsContainer -eq $False -and $_.Extension -eq "a" } | `
+    Where-Object { $_.PSIsContainer -eq $False -and $_.Extension -eq ".a" } | `
     ForEach-Object {Copy-Item -Path $_.Fullname -Destination $dest -Force} # Do the things
 Copy-Item -Path "$source/C_header/okapi.h" -Destination "$dest/include" -Force
 Install-Requirements
