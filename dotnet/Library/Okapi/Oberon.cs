814515cdc329fdaacd64489c275c6d9ea5916970
Tomislav Markovski <tomislav@trinsic.id>
2021-10-08 11:54:35 -0400

    Add tests with blinding

--- dotnet/Library/Okapi/Oberon.cs
+++ dotnet/Library/Okapi/Oberon.cs
@@ -2,22 +2,22 @@ namespace Okapi.Security
 {
     public class Oberon
     {
-        public static CreateOberonKeyReply CreateKey(CreateOberonKeyRequest request) =>
-            Native.Call<CreateOberonKeyRequest, CreateOberonKeyReply>(request, Native.oberon_create_key);
+        public static CreateOberonKeyResponse CreateKey(CreateOberonKeyRequest request) =>
+            Native.Call<CreateOberonKeyRequest, CreateOberonKeyResponse>(request, Native.oberon_create_key);
 
-        public static CreateOberonTokenReply CreateToken(CreateOberonTokenRequest request) =>
-            Native.Call<CreateOberonTokenRequest, CreateOberonTokenReply>(request, Native.oberon_create_token);
+        public static CreateOberonTokenResponse CreateToken(CreateOberonTokenRequest request) =>
+            Native.Call<CreateOberonTokenRequest, CreateOberonTokenResponse>(request, Native.oberon_create_token);
 
-        public static CreateOberonProofReply CreateProof(CreateOberonProofRequest request) =>
-            Native.Call<CreateOberonProofRequest, CreateOberonProofReply>(request, Native.oberon_create_proof);
+        public static CreateOberonProofResponse CreateProof(CreateOberonProofRequest request) =>
+            Native.Call<CreateOberonProofRequest, CreateOberonProofResponse>(request, Native.oberon_create_proof);
 
-        public static VerifyOberonProofReply VerifyProof(VerifyOberonProofRequest request) =>
-            Native.Call<VerifyOberonProofRequest, VerifyOberonProofReply>(request, Native.oberon_verify_proof);
+        public static VerifyOberonProofResponse VerifyProof(VerifyOberonProofRequest request) =>
+            Native.Call<VerifyOberonProofRequest, VerifyOberonProofResponse>(request, Native.oberon_verify_proof);
 
-        public static BlindOberonTokenReply BlindToken(BlindOberonTokenRequest request) =>
-            Native.Call<BlindOberonTokenRequest, BlindOberonTokenReply>(request, Native.oberon_blind_token);
+        public static BlindOberonTokenResponse BlindToken(BlindOberonTokenRequest request) =>
+            Native.Call<BlindOberonTokenRequest, BlindOberonTokenResponse>(request, Native.oberon_blind_token);
 
-        public static UnBlindOberonTokenReply UnblindToken(UnBlindOberonTokenRequest request) =>
-            Native.Call<UnBlindOberonTokenRequest, UnBlindOberonTokenReply>(request, Native.oberon_unblind_token);
+        public static UnBlindOberonTokenResponse UnblindToken(UnBlindOberonTokenRequest request) =>
+            Native.Call<UnBlindOberonTokenRequest, UnBlindOberonTokenResponse>(request, Native.oberon_unblind_token);
     }
 }
--- dotnet/Tests/Okapi.Tests/OberonTests.cs
+++ dotnet/Tests/Okapi.Tests/OberonTests.cs
@@ -37,5 +37,104 @@ namespace Okapi.Tests
 
             Assert.True(result.Valid);
         }
+
+        [Fact]
+        public void TestDemoWithBlinding()
+        {
+            // Issuer generates oberon key pair
+            var key = Oberon.CreateKey(new CreateOberonKeyRequest());
+
+            var data = ByteString.CopyFromUtf8("alice");
+            var nonce = ByteString.CopyFromUtf8("1234");
+
+            // blinding code to be used by issuer and given to holder
+            // to transfer the token securely
+            var issuer_2fa = ByteString.CopyFromUtf8("issuer code");
+
+            CreateOberonTokenRequest tokenRequst = new()
+            {
+                Data = data,
+                Sk = key.Sk
+            };
+            tokenRequst.Blinding.Add(issuer_2fa);
+
+            var blindedToken = Oberon.CreateToken(tokenRequst);
+
+            // Holder unblinds the token
+            UnBlindOberonTokenRequest unblindRequest = new() { Token = blindedToken.Token };
+            unblindRequest.Blinding.Add(issuer_2fa);
+
+            var token = Oberon.UnblindToken(unblindRequest);
+
+            // Holder prepares a proof without blinding
+            var proof = Oberon.CreateProof(new CreateOberonProofRequest
+            {
+                Data = data,
+                Nonce = nonce,
+                Token = token.Token
+            });
+
+            // Verifier verifies the proof
+            var result = Oberon.VerifyProof(new VerifyOberonProofRequest
+            {
+                Data = data,
+                Nonce = nonce,
+                Pk = key.Pk,
+                Proof = proof.Proof
+            });
+
+            Assert.True(result.Valid);
+
+            // Holder blinds the token with a personal pin
+            var userPin = ByteString.CopyFromUtf8("0042");
+            BlindOberonTokenRequest blindRequest = new() { Token = token.Token };
+            blindRequest.Blinding.Add(userPin);
+
+            var userBlindedToken = Oberon.BlindToken(blindRequest);
+
+            // Holder prepares a proof using the pin blinding
+            CreateOberonProofRequest proofRequest = new()
+            {
+                Data = data,
+                Nonce = nonce,
+                Token = userBlindedToken.Token
+            };
+            proofRequest.Blinding.Add(userPin);
+
+            proof = Oberon.CreateProof(proofRequest);
+
+            // Verifier verifies the proof
+            result = Oberon.VerifyProof(new VerifyOberonProofRequest
+            {
+                Data = data,
+                Nonce = nonce,
+                Pk = key.Pk,
+                Proof = proof.Proof
+            });
+
+            Assert.True(result.Valid);
+
+            // Bad actor creates a proof with incorrect blinding pin
+            proofRequest = new()
+            {
+                Data = data,
+                Nonce = nonce,
+                Token = userBlindedToken.Token
+            };
+            proofRequest.Blinding.Add(ByteString.CopyFromUtf8("invalid pin"));
+
+            proof = Oberon.CreateProof(proofRequest);
+
+            // Verifier tries to verify proof, fails
+            result = Oberon.VerifyProof(new VerifyOberonProofRequest
+            {
+                Data = data,
+                Nonce = nonce,
+                Pk = key.Pk,
+                Proof = proof.Proof
+            });
+
+            Assert.False(result.Valid);
+        }
     }
 }
