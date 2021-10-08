a15c1f39ec654031a06c42f205de4f1b16504321
Scott Phillips <polygonguru@gmail.com>
2021-10-08 11:39:41 -0400

    Golang oberon (#194)

--- go/okapi/DidKey.go
+++ go/okapi/DidKey.go
@@ -1,21 +1,21 @@
 package okapi
 
-import "github.com/trinsic-id/okapi/go/proto"
+import "github.com/trinsic-id/okapi/go/okapi_proto"
 
 type DidKey struct {}
 type DidKeyer interface {
-	Generate(request *proto.GenerateKeyRequest) (*proto.GenerateKeyResponse, error)
-	Resolve(request *proto.ResolveRequest) (*proto.ResolveResponse, error)
+	Generate(request *okapi.GenerateKeyRequest) (*okapi.GenerateKeyResponse, error)
+	Resolve(request *okapi.ResolveRequest) (*okapi.ResolveResponse, error)
 }
 
-func (d DidKey) Generate(request *proto.GenerateKeyRequest) (*proto.GenerateKeyResponse, error) {
-	response := proto.GenerateKeyResponse{}
+func (d DidKey) Generate(request *okapi.GenerateKeyRequest) (*okapi.GenerateKeyResponse, error) {
+	response := okapi.GenerateKeyResponse{}
 	err := callOkapiNative(request, &response, didkeyGenerate)
 	return &response, err
 }
 
-func (d DidKey) Resolve(request *proto.ResolveRequest) (*proto.ResolveResponse, error) {
-	response := proto.ResolveResponse{}
+func (d DidKey) Resolve(request *okapi.ResolveRequest) (*okapi.ResolveResponse, error) {
+	response := okapi.ResolveResponse{}
 	err := callOkapiNative(request, &response, didkeyResolve)
 	return &response, err
 }
