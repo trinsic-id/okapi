0edfba587ed2528a0ba72804d243c04fa0b23830
Scott Phillips <polygonguru@gmail.com>
2021-10-13 15:05:04 -0400

    Java 8 backwards compatible (#198)

--- java/src/main/java/Oberon.java
+++ java/src/main/java/Oberon.java
@@ -1,58 +1,58 @@
 import com.google.protobuf.InvalidProtocolBufferException;
-import trinsic.okapi.Security;
+import trinsic.okapi.security.*;
 
 public class Oberon extends OkapiNative {
-    public static Security.CreateOberonKeyResponse createKey(Security.CreateOberonKeyRequest request) throws DidException, InvalidProtocolBufferException {
-        var requestBuffer = messageToBuffer(request);
-        var responseBuffer = new OkapiByteBuffer();
-        var errBuffer = new ExternError();
+    public static CreateOberonKeyResponse createKey(CreateOberonKeyRequest request) throws DidException, InvalidProtocolBufferException {
+        OkapiByteBuffer.ByValue requestBuffer = messageToBuffer(request);
+        OkapiByteBuffer responseBuffer = new OkapiByteBuffer();
+        ExternError errBuffer = new ExternError();
         getNativeLibrary().oberon_create_key(requestBuffer, responseBuffer, errBuffer);
         errBuffer.RaiseError();
-        return Security.CreateOberonKeyResponse.parseFrom(bufferToByteArray(responseBuffer));
+        return CreateOberonKeyResponse.parseFrom(bufferToByteArray(responseBuffer));
     }
 
-    public static Security.CreateOberonTokenResponse createToken(Security.CreateOberonTokenRequest request) throws DidException, InvalidProtocolBufferException {
-        var requestBuffer = messageToBuffer(request);
-        var responseBuffer = new OkapiByteBuffer();
-        var errBuffer = new ExternError();
+    public static CreateOberonTokenResponse createToken(CreateOberonTokenRequest request) throws DidException, InvalidProtocolBufferException {
+        OkapiByteBuffer.ByValue requestBuffer = messageToBuffer(request);
+        OkapiByteBuffer responseBuffer = new OkapiByteBuffer();
+        ExternError errBuffer = new ExternError();
         getNativeLibrary().oberon_create_token(requestBuffer, responseBuffer, errBuffer);
         errBuffer.RaiseError();
-        return Security.CreateOberonTokenResponse.parseFrom(bufferToByteArray(responseBuffer));
+        return CreateOberonTokenResponse.parseFrom(bufferToByteArray(responseBuffer));
     }
 
-    public static Security.BlindOberonTokenResponse blindToken(Security.BlindOberonTokenRequest request) throws DidException, InvalidProtocolBufferException {
-        var requestBuffer = messageToBuffer(request);
-        var responseBuffer = new OkapiByteBuffer();
-        var errBuffer = new ExternError();
+    public static BlindOberonTokenResponse blindToken(BlindOberonTokenRequest request) throws DidException, InvalidProtocolBufferException {
+        OkapiByteBuffer.ByValue requestBuffer = messageToBuffer(request);
+        OkapiByteBuffer responseBuffer = new OkapiByteBuffer();
+        ExternError errBuffer = new ExternError();
         getNativeLibrary().oberon_blind_token(requestBuffer, responseBuffer, errBuffer);
         errBuffer.RaiseError();
-        return Security.BlindOberonTokenResponse.parseFrom(bufferToByteArray(responseBuffer));
+        return BlindOberonTokenResponse.parseFrom(bufferToByteArray(responseBuffer));
     }
 
-    public static Security.UnBlindOberonTokenResponse unBlindToken(Security.UnBlindOberonTokenRequest request) throws DidException, InvalidProtocolBufferException {
-        var requestBuffer = messageToBuffer(request);
-        var responseBuffer = new OkapiByteBuffer();
-        var errBuffer = new ExternError();
+    public static UnBlindOberonTokenResponse unBlindToken(UnBlindOberonTokenRequest request) throws DidException, InvalidProtocolBufferException {
+        OkapiByteBuffer.ByValue requestBuffer = messageToBuffer(request);
+        OkapiByteBuffer responseBuffer = new OkapiByteBuffer();
+        ExternError errBuffer = new ExternError();
         getNativeLibrary().oberon_unblind_token(requestBuffer, responseBuffer, errBuffer);
         errBuffer.RaiseError();
-        return Security.UnBlindOberonTokenResponse.parseFrom(bufferToByteArray(responseBuffer));
+        return UnBlindOberonTokenResponse.parseFrom(bufferToByteArray(responseBuffer));
     }
 
-    public static Security.CreateOberonProofResponse createProof(Security.CreateOberonProofRequest request) throws DidException, InvalidProtocolBufferException {
-        var requestBuffer = messageToBuffer(request);
-        var responseBuffer = new OkapiByteBuffer();
-        var errBuffer = new ExternError();
+    public static CreateOberonProofResponse createProof(CreateOberonProofRequest request) throws DidException, InvalidProtocolBufferException {
+        OkapiByteBuffer.ByValue requestBuffer = messageToBuffer(request);
+        OkapiByteBuffer responseBuffer = new OkapiByteBuffer();
+        ExternError errBuffer = new ExternError();
         getNativeLibrary().oberon_create_proof(requestBuffer, responseBuffer, errBuffer);
         errBuffer.RaiseError();
-        return Security.CreateOberonProofResponse.parseFrom(bufferToByteArray(responseBuffer));
+        return CreateOberonProofResponse.parseFrom(bufferToByteArray(responseBuffer));
     }
 
-    public static Security.VerifyOberonProofResponse verifyProof(Security.VerifyOberonProofRequest request) throws DidException, InvalidProtocolBufferException {
-        var requestBuffer = messageToBuffer(request);
-        var responseBuffer = new OkapiByteBuffer();
-        var errBuffer = new ExternError();
+    public static VerifyOberonProofResponse verifyProof(VerifyOberonProofRequest request) throws DidException, InvalidProtocolBufferException {
+        OkapiByteBuffer.ByValue requestBuffer = messageToBuffer(request);
+        OkapiByteBuffer responseBuffer = new OkapiByteBuffer();
+        ExternError errBuffer = new ExternError();
         getNativeLibrary().oberon_verify_proof(requestBuffer, responseBuffer, errBuffer);
         errBuffer.RaiseError();
-        return Security.VerifyOberonProofResponse.parseFrom(bufferToByteArray(responseBuffer));
+        return VerifyOberonProofResponse.parseFrom(bufferToByteArray(responseBuffer));
     }
 }
