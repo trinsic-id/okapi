3a97f7323956743b30012b19100fa46c9472fc8a
Scott Phillips <polygonguru@gmail.com>
2021-10-13 14:28:12 -0400

    ARMv7 support (#199)

--- native/src/proto/okapi_examples.rs
+++
@@ -1,6 +0,0 @@
-#[derive(::serde::Serialize, ::serde::Deserialize)]
-#[derive(Clone, PartialEq, ::prost::Message)]
-pub struct BasicMessage {
-    #[prost(string, tag="1")]
-    pub text: ::prost::alloc::string::String,
-}
