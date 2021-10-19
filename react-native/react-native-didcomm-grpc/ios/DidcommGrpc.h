876645f29137d15a2ec60c6b9630985e2db11ac7
Tomislav Markovski <tomislav@trinsic.id>
2021-10-19 14:21:16 -0400

    Remove react-native code

--- react-native/react-native-didcomm-grpc/ios/DidcommGrpc.h
+++
@@ -1,9 +0,0 @@
-#import <React/RCTBridgeModule.h>
-
-@interface DidcommGrpc : NSObject <RCTBridgeModule>
-
-@end
-
-@interface DIDKeyGrpc : NSObject <RCTBridgeModule>
-
-@end
--- react-native/react-native-didcomm-grpc/src/__tests__/index.test.tsx
+++
@@ -1,6 +0,0 @@
-it('should serialize', () => {
-  let a: Uint8Array = Uint8Array.from([1, 2, 3]);
-  let b: number[] = Array.from<number>(a);
-
-  expect(b).toStrictEqual([1, 2, 3]);
-});
