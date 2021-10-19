122ed116ec39716a6bc22e872e72621bdb33ed15
Scott Phillips <polygonguru@gmail.com>
2021-10-19 08:48:11 -0400

    Build and test .net

--- dotnet/BuildAndTest.ps1
+++ dotnet/BuildAndTest.ps1
@@ -1,5 +1,8 @@
 # Runs the tests and collects coverage results
-
+dotnet build
+if ($LASTEXITCODE -lt 0) {
+    exit $LASTEXITCODE
+}
 dotnet test --% --logger "junit;LogFilePath=TestResults/test_output.xml" /p:CollectCoverage=true /p:CoverletOutput=TestResults/ /p:CoverletOutputFormat=\"cobertura,json\" /p:ExcludeByFile=\"**/obj/**/*.cs\"
 if ($LASTEXITCODE -ge 0 -Or $LASTEXITCODE -le 1) {
     exit 0
