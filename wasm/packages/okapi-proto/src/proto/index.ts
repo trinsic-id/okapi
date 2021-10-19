11ffb47abe791c3a039c2a69104edef21b6eb19b
Tomislav Markovski <tomislav@trinsic.id>
2021-10-19 11:06:38 -0400

    Update wasm

---
+++ wasm/packages/okapi-proto/src/proto/index.ts
@@ -0,0 +1,6 @@
+export * from "./okapi/examples/v1/examples_pb";
+export * from "./okapi/keys/v1/keys_pb";
+export * from "./okapi/proofs/v1/proofs_pb";
+export * from "./okapi/security/v1/security_pb";
+export * from "./okapi/transport/v1/transport_pb";
+export * from "./pbmse/v1/pbmse_pb";
