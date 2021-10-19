217d26fa3d77e82578a3c561cf76c9c630b6e71b
Tomislav Markovski <tmarkovski@gmail.com>
2021-10-19 11:26:56 -0400

    Add buf setup step (#209)

--- wasm/packages/okapi/test/node.test.js
+++ wasm/packages/okapi/test/node.test.js
@@ -2,7 +2,7 @@ const test = require("ava");
 const { DIDKey, GenerateKeyRequest, KeyType } = require("../lib/index.js");
 
 test("generate bls key", async (t) => {
-  var response = await DIDKey.generate(new GenerateKeyRequest().setKeyType(KeyType.BLS12381G1G2));
+  var response = await DIDKey.generate(new GenerateKeyRequest().setKeyType(KeyType.KEY_TYPE_BLS12381G1G2));
 
   t.not(null, response);
   t.not(undefined, response);
