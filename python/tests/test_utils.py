f39c0debc5403d031360af8ed3a8d0b5f6b1f65d
Scott Phillips <polygonguru@gmail.com>
2021-10-29 18:58:41 -0400

    optimize the imports

--- python/tests/test_utils.py
+++ python/tests/test_utils.py
@@ -1,13 +1,4 @@
-import base64
 import unittest
-from os.path import dirname, join, abspath
-
-import base58
-
-import okapi.okapi_utils
-from okapi.wrapper import DIDKey
-from okapi.proto.okapi.keys.v1 import GenerateKeyRequest, KeyType, GenerateKeyResponse, ResolveRequest
-from okapi.okapi_utils import DidError, get_os_arch_binary, set_library_path
 
 
 class UtilitiesTests(unittest.TestCase):
