b78d38fead4c0ee4df4824ab7470d8fc3b84a957
Scott Phillips <polygonguru@gmail.com>
2021-10-29 19:03:27 -0400

    Only download binaries if needed for Python (#240)

---
+++ python/tests/test_utils.py
@@ -0,0 +1,12 @@
+import unittest
+
+
+class UtilitiesTests(unittest.TestCase):
+    def test_download_binaries(self):
+        # Only run this locally, it should take 20-30 seconds the first run, and instantaneous on the second.
+        # okapi.okapi_utils.download_binaries(False)
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
