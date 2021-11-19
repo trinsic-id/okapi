ee21e13fffa49141be1a701887a8a79093cd1c34
Scott Phillips <polygonguru@gmail.com>
2021-11-19 13:11:13 -0500

    Java free native strings (#267)

--- java/src/main/java/ExternError.java
+++ java/src/main/java/ExternError.java
@@ -1,14 +1,18 @@
+import com.sun.jna.Pointer;
 import com.sun.jna.Structure;
 import com.sun.jna.Structure.FieldOrder;
 
 @FieldOrder({"code", "message"})
 public class ExternError extends Structure {
     public int code;
-    public String message;
+    public Pointer message;
 
     public void RaiseError() throws DidException {
         if (this.code == 0)
             return;
-        throw new DidException(this.code, this.message);
+        // Create the string, then dump the backing store.
+        var messageString = this.message.getString(0, "UTF-8");
+        OkapiNative.getNativeLibrary().okapi_string_free(this.message);
+        throw new DidException(this.code, messageString);
     }
 }
