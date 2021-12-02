7530f84e613b14feaba2cb67b86f5e156f34750d
Tomislav Markovski <tomislav@trinsic.id>
2021-12-02 12:06:50 -0500

    Add missing outputs to pr validation action (#289)
    * Add missing outputs to pr validation action
    
    * Add prettier for wasm

--- wasm/packages/okapi/src/index.web.ts
+++ wasm/packages/okapi/src/index.web.ts
@@ -11,15 +11,23 @@ async function initialize() {
 }
 
 export class DIDKey {
-  static async generate(request: proto.GenerateKeyRequest): Promise<proto.GenerateKeyResponse> {
+  static async generate(
+    request: proto.GenerateKeyRequest
+  ): Promise<proto.GenerateKeyResponse> {
     await initialize();
 
-    return proto.GenerateKeyResponse.deserializeBinary(native.didkey_generate(request.serializeBinary()));
+    return proto.GenerateKeyResponse.deserializeBinary(
+      native.didkey_generate(request.serializeBinary())
+    );
   }
-  static async resolve(request: proto.ResolveRequest): Promise<proto.ResolveResponse> {
+  static async resolve(
+    request: proto.ResolveRequest
+  ): Promise<proto.ResolveResponse> {
     await initialize();
 
-    return proto.ResolveResponse.deserializeBinary(native.didkey_resolve(request.serializeBinary()));
+    return proto.ResolveResponse.deserializeBinary(
+      native.didkey_resolve(request.serializeBinary())
+    );
   }
 }
 
@@ -27,61 +35,105 @@ export class DIDComm {
   static async pack(request: proto.PackRequest): Promise<proto.PackResponse> {
     await initialize();
 
-    return proto.PackResponse.deserializeBinary(native.didcomm_pack(request.serializeBinary()));
+    return proto.PackResponse.deserializeBinary(
+      native.didcomm_pack(request.serializeBinary())
+    );
   }
-  static async unpack(request: proto.UnpackRequest): Promise<proto.UnpackResponse> {
+  static async unpack(
+    request: proto.UnpackRequest
+  ): Promise<proto.UnpackResponse> {
     await initialize();
 
-    return proto.UnpackResponse.deserializeBinary(native.didcomm_unpack(request.serializeBinary()));
+    return proto.UnpackResponse.deserializeBinary(
+      native.didcomm_unpack(request.serializeBinary())
+    );
   }
   static async sign(request: proto.SignRequest): Promise<proto.SignResponse> {
     await initialize();
 
-    return proto.SignResponse.deserializeBinary(native.didcomm_sign(request.serializeBinary()));
+    return proto.SignResponse.deserializeBinary(
+      native.didcomm_sign(request.serializeBinary())
+    );
   }
-  static async verify(request: proto.VerifyRequest): Promise<proto.VerifyResponse> {
+  static async verify(
+    request: proto.VerifyRequest
+  ): Promise<proto.VerifyResponse> {
     await initialize();
 
-    return proto.VerifyResponse.deserializeBinary(native.didcomm_verify(request.serializeBinary()));
+    return proto.VerifyResponse.deserializeBinary(
+      native.didcomm_verify(request.serializeBinary())
+    );
   }
 }
 
 export class LdProofs {
-  static async generate(request: proto.CreateProofRequest): Promise<proto.CreateProofResponse> {
+  static async generate(
+    request: proto.CreateProofRequest
+  ): Promise<proto.CreateProofResponse> {
     await initialize();
 
-    return proto.CreateProofResponse.deserializeBinary(native.ldproofs_create_proof(request.serializeBinary()));
+    return proto.CreateProofResponse.deserializeBinary(
+      native.ldproofs_create_proof(request.serializeBinary())
+    );
   }
-  static async convert(request: proto.VerifyProofRequest): Promise<proto.VerifyProofResponse> {
+  static async convert(
+    request: proto.VerifyProofRequest
+  ): Promise<proto.VerifyProofResponse> {
     await initialize();
 
-    return proto.VerifyProofResponse.deserializeBinary(native.ldproofs_verify_proof(request.serializeBinary()));
+    return proto.VerifyProofResponse.deserializeBinary(
+      native.ldproofs_verify_proof(request.serializeBinary())
+    );
   }
 }
 
 export class Oberon {
-  static async createKey(request: proto.CreateOberonKeyRequest): Promise<proto.CreateOberonKeyResponse> {
+  static async createKey(
+    request: proto.CreateOberonKeyRequest
+  ): Promise<proto.CreateOberonKeyResponse> {
     await initialize();
-    return proto.CreateOberonKeyResponse.deserializeBinary(native.oberon_create_key(request.serializeBinary()));
+    return proto.CreateOberonKeyResponse.deserializeBinary(
+      native.oberon_create_key(request.serializeBinary())
+    );
   }
-  static async createToken(request: proto.CreateOberonTokenRequest): Promise<proto.CreateOberonTokenResponse> {
+  static async createToken(
+    request: proto.CreateOberonTokenRequest
+  ): Promise<proto.CreateOberonTokenResponse> {
     await initialize();
-    return proto.CreateOberonTokenResponse.deserializeBinary(native.oberon_create_token(request.serializeBinary()));
+    return proto.CreateOberonTokenResponse.deserializeBinary(
+      native.oberon_create_token(request.serializeBinary())
+    );
   }
-  static async createProof(request: proto.CreateOberonProofRequest): Promise<proto.CreateOberonProofResponse> {
+  static async createProof(
+    request: proto.CreateOberonProofRequest
+  ): Promise<proto.CreateOberonProofResponse> {
     await initialize();
-    return proto.CreateOberonProofResponse.deserializeBinary(native.oberon_create_proof(request.serializeBinary()));
+    return proto.CreateOberonProofResponse.deserializeBinary(
+      native.oberon_create_proof(request.serializeBinary())
+    );
   }
-  static async verifyProof(request: proto.VerifyOberonProofRequest): Promise<proto.VerifyOberonProofResponse> {
+  static async verifyProof(
+    request: proto.VerifyOberonProofRequest
+  ): Promise<proto.VerifyOberonProofResponse> {
     await initialize();
-    return proto.VerifyOberonProofResponse.deserializeBinary(native.oberon_verify_proof(request.serializeBinary()));
+    return proto.VerifyOberonProofResponse.deserializeBinary(
+      native.oberon_verify_proof(request.serializeBinary())
+    );
   }
-  static async blindToken(request: proto.BlindOberonTokenRequest): Promise<proto.BlindOberonTokenResponse> {
+  static async blindToken(
+    request: proto.BlindOberonTokenRequest
+  ): Promise<proto.BlindOberonTokenResponse> {
     await initialize();
-    return proto.BlindOberonTokenResponse.deserializeBinary(native.oberon_blind_token(request.serializeBinary()));
+    return proto.BlindOberonTokenResponse.deserializeBinary(
+      native.oberon_blind_token(request.serializeBinary())
+    );
   }
-  static async unblindToken(request: proto.UnBlindOberonTokenRequest): Promise<proto.UnBlindOberonTokenResponse> {
+  static async unblindToken(
+    request: proto.UnBlindOberonTokenRequest
+  ): Promise<proto.UnBlindOberonTokenResponse> {
     await initialize();
-    return proto.UnBlindOberonTokenResponse.deserializeBinary(native.oberon_unblind_token(request.serializeBinary()));
+    return proto.UnBlindOberonTokenResponse.deserializeBinary(
+      native.oberon_unblind_token(request.serializeBinary())
+    );
   }
 }
