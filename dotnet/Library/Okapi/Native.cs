70145e88c406b9c619e3874e236816bfde1b346b
Tomislav Markovski <tomislav@trinsic.id>
2021-12-02 12:47:14 -0500

    Move dotnet dependency to net6.0 (#291)
    * Use net6.0 tfms
    
    * Support browser wasm
    
    * Small adjustments
    
    * Fix release
    
    * Install dotnet 6
    
    * Build without script
    
    * Revert "Build without script"
    
    This reverts commit c30bcc2c7ff566f4d492cda5acc3f4aff91db498.
    
    * Use current branch action
    
    * install workload
    
    * Remove matrix

--- dotnet/Library/Okapi/Native.cs
+++ dotnet/Library/Okapi/Native.cs
@@ -1,106 +1,102 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
-using System.Text;
 using Google.Protobuf;
 
-namespace Okapi
+namespace Okapi;
+
+internal class Native
 {
-    internal class Native
-    {
 #if __IOS__
-        private const string LibraryName = "__Internal";
+    private const string LibraryName = "__Internal";
 #else
-        private const string LibraryName = "okapi";
+    private const string LibraryName = "okapi";
 #endif
 
-        internal delegate int NativeMethod(ByteBuffer request, out ByteBuffer response, out ExternError error);
+    internal delegate int NativeMethod(ByteBuffer request, out ByteBuffer response, out ExternError error);
 
-        internal static TResponse Call<TRequest, TResponse>(TRequest request, NativeMethod nativeMethod)
-            where TRequest : IMessage
-            where TResponse : IMessage, new()
-        {
-            using var memory = new UnmanagedMemory();
+    internal static TResponse Call<TRequest, TResponse>(TRequest request, NativeMethod nativeMethod)
+        where TRequest : IMessage
+        where TResponse : IMessage, new()
+    {
+        using var memory = new UnmanagedMemory();
 
-            var code = nativeMethod(memory.ToByteBuffer(request.ToByteArray()), out var response, out var error);
-            memory.ThrowOnError(error);
+        var code = nativeMethod(memory.ToByteBuffer(request.ToByteArray()), out var response, out var error);
+        memory.ThrowOnError(error);
 
-            var res = new TResponse();
-            res.MergeFrom(memory.ToArray(response));
-            return res;
-        }
-
-        #region Resources
-
-        [DllImport(LibraryName, CharSet = CharSet.Ansi, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int okapi_string_free(IntPtr str);
-
-        [DllImport(LibraryName, CharSet = CharSet.Ansi, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int okapi_byte_buffer_free(ByteBuffer data);
-
-        #endregion
-
-        #region Keys
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int didkey_generate(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int didkey_resolve(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        #endregion
-
-        #region Pack
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int didcomm_pack(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int didcomm_unpack(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        #endregion
-
-        #region Sign
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int didcomm_sign(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int didcomm_verify(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        #endregion
-
-
-        #region Ld Proofs
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int ldproofs_create_proof(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int ldproofs_verify_proof(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        #endregion
-
-        #region Oberon
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int oberon_create_key(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int oberon_create_token(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int oberon_create_proof(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int oberon_verify_proof(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int oberon_blind_token(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
-        internal static extern int oberon_unblind_token(ByteBuffer request, out ByteBuffer response, out ExternError error);
-
-        #endregion
+        var res = new TResponse();
+        res.MergeFrom(memory.ToArray(response));
+        return res;
     }
+
+    #region Resources
+
+    [DllImport(LibraryName, CharSet = CharSet.Ansi, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern void okapi_string_free(IntPtr str);
+
+    [DllImport(LibraryName, CharSet = CharSet.Ansi, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern void okapi_bytebuffer_free(ByteBuffer data);
+
+    #endregion
+
+    #region Keys
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int didkey_generate(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int didkey_resolve(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    #endregion
+
+    #region Pack
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int didcomm_pack(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int didcomm_unpack(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    #endregion
+
+    #region Sign
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int didcomm_sign(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int didcomm_verify(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    #endregion
+
+
+    #region Ld Proofs
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int ldproofs_create_proof(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int ldproofs_verify_proof(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    #endregion
+
+    #region Oberon
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int oberon_create_key(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int oberon_create_token(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int oberon_create_proof(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int oberon_verify_proof(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int oberon_blind_token(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    [DllImport(LibraryName, CharSet = CharSet.Auto, BestFitMapping = false, ThrowOnUnmappableChar = true)]
+    internal static extern int oberon_unblind_token(ByteBuffer request, out ByteBuffer response, out ExternError error);
+
+    #endregion
 }
