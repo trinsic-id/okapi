fc9324910be7c6b8f22007907c4c1e114a33b742
Scott Phillips <polygonguru@gmail.com>
2021-12-29 07:32:20 -0500

    store library path for jna in macos (#320)

--- java/src/main/java/trinsic/okapi/OkapiNative.java
+++ java/src/main/java/trinsic/okapi/OkapiNative.java
@@ -11,6 +11,76 @@ import java.nio.file.Paths;
 
 public class OkapiNative {
 
+    private static IOkapiC nativeLibrary = null;
+    private static String overrideLibraryPath = null;
+
+    public synchronized static IOkapiC getNativeLibrary() {
+        if (nativeLibrary == null) {
+            var libPath = getLibraryPath();
+            System.setProperty("jna.library.path", libPath);
+            System.out.println("Native.load(" + libPath + ")");
+            nativeLibrary = Native.load(libPath, IOkapiC.class);
+        }
+
+        return nativeLibrary;
+    }
+
+    private static String getLibraryName() {
+        if (Platform.isWindows())
+            return "okapi.dll";
+        if (Platform.isMac())
+            return "libokapi.dylib";
+        // Default is linux
+        return "libokapi.so";
+    }
+
+    public static String getLibraryPath() {
+        // Explicit path to entire library name
+        if (overrideLibraryPath != null && overrideLibraryPath.strip().length() > 0)
+            return Paths.get(overrideLibraryPath).toAbsolutePath().toString();
+
+        var okapi_lib_path = getLibPath("LD_LIBRARY_PATH", "");
+        okapi_lib_path = getLibPath("DYLD_LIBRARY_PATH", okapi_lib_path);
+        // Depending on if System Integrity Protection is enabled, MacOS won't allow the above environment variables.
+        // https://stackoverflow.com/a/60128194
+        okapi_lib_path = getLibPath("JAVA_LIBRARY_PATH", okapi_lib_path);
+        // Macos makes things needlessly hard
+        if (okapi_lib_path != null && okapi_lib_path.strip().length() > 0) {
+            for (var path : okapi_lib_path.split(File.pathSeparator)) {
+                var testPath = Paths.get(path, getLibraryName()).toAbsolutePath();
+                System.out.println("test path=" + testPath);
+                if (Files.exists(testPath))
+                    return testPath.toString();
+            }
+        }
+        // System native path load
+        return "okapi";
+    }
+
+    public static void setLibraryPath(String path) {
+        overrideLibraryPath = path;
+    }
+
+    private static String getLibPath(String envVar, String okapi_lib_path) {
+        if (okapi_lib_path == null || okapi_lib_path.strip().length() == 0) {
+            okapi_lib_path = System.getenv(envVar);
+            System.out.println(envVar + "=" + okapi_lib_path);
+        }
+        return okapi_lib_path;
+    }
+
+    static OkapiByteBuffer.ByValue messageToBuffer(GeneratedMessageV3 requestMessage) {
+        OkapiByteBuffer.ByValue requestBuffer = new OkapiByteBuffer.ByValue();
+        requestBuffer.setData(requestMessage.toByteArray());
+        return requestBuffer;
+    }
+
+    static byte[] bufferToByteArray(OkapiByteBuffer buffer) {
+        byte[] data = buffer.getData();
+        getNativeLibrary().okapi_bytebuffer_free(buffer.byValue());
+        return data;
+    }
+
     public interface IOkapiC extends com.sun.jna.Library {
         int didcomm_pack(OkapiByteBuffer.ByValue request, OkapiByteBuffer response, ExternError err);
 
@@ -43,55 +113,5 @@ public class OkapiNative {
         void okapi_bytebuffer_free(OkapiByteBuffer.ByValue v);
 
         void okapi_string_free(com.sun.jna.Pointer s);
-    }
-
-    private static IOkapiC nativeLibrary = null;
-    public synchronized static IOkapiC getNativeLibrary() {
-        if (nativeLibrary == null)
-            nativeLibrary = Native.load(getLibraryPath(), IOkapiC.class);
-
-        return nativeLibrary;
-    }
-
-    private static String getLibraryName() {
-        if (Platform.isWindows())
-            return "okapi.dll";
-        if (Platform.isMac())
-            return "libokapi.dylib";
-        // Default is linux
-        return "libokapi.so";
-    }
-
-    private static String overrideLibraryPath = null;
-    public static void setLibraryPath(String path) { overrideLibraryPath = path; }
-    public static String getLibraryPath() {
-        // Explicit path to entire library name
-        if (overrideLibraryPath != null && overrideLibraryPath.strip().length() > 0)
-            return Paths.get(overrideLibraryPath).toAbsolutePath().toString();
-
-        var okapi_lib_path = System.getenv("LD_LIBRARY_PATH");
-        System.out.println("LD_LIBRARY_PATH=" + okapi_lib_path);
-        if (okapi_lib_path != null && okapi_lib_path.strip().length() > 0) {
-            for (var path: okapi_lib_path.split(File.pathSeparator)) {
-                var testPath = Paths.get(path, getLibraryName()).toAbsolutePath();
-                System.out.println("test path=" + testPath);
-                if (Files.exists(testPath))
-                    return testPath.toString();
-            }
-        }
-        // System native path load
-        return "okapi";
-    }
-
-    static OkapiByteBuffer.ByValue messageToBuffer(GeneratedMessageV3 requestMessage) {
-        OkapiByteBuffer.ByValue requestBuffer = new OkapiByteBuffer.ByValue();
-        requestBuffer.setData(requestMessage.toByteArray());
-        return requestBuffer;
-    }
-
-    static byte[] bufferToByteArray(OkapiByteBuffer buffer) {
-        byte[] data = buffer.getData();
-        getNativeLibrary().okapi_bytebuffer_free(buffer.byValue());
-        return data;
     }
 }
