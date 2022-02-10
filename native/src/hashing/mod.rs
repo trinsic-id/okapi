38e32ea46551cef332080e1152d5ad39d3410cb6
Scott Phillips <polygonguru@gmail.com>
2022-02-10 08:44:24 -0500

    message updates

--- native/src/hashing/mod.rs
+++ native/src/hashing/mod.rs
@@ -13,14 +13,14 @@ impl crate::Hashing {
         })
     }
 
-    pub fn blake3_keyed_hash<'a>(request: &Blake3HashRequest) -> Result<Blake3HashResponse, Error<'a>> {
-        let key = request.key.clone().unwrap();
+    pub fn blake3_keyed_hash<'a>(request: &Blake3KeyedHashRequest) -> Result<Blake3KeyedHashResponse, Error<'a>> {
+        let key = request.key.clone();
         // let key_vec =  unwrap_or_return!(key_chunk.next(), Error::Message("BLAKE3 key must be 32 bytes"));
         let key_slice = unwrap_or_return!(<&[u8; 32]>::try_from(key.as_slice()).ok(), Error::Message("BLAKE3 key must be 32 bytes"));
         let data = request.data.clone();
         // Hash the provided data
         let hash1 = blake3::keyed_hash(key_slice, data.as_slice());
-        Ok(Blake3HashResponse {
+        Ok(Blake3KeyedHashResponse {
             output: hash1.as_bytes().to_vec(),
         })
     }
@@ -39,14 +39,13 @@ impl crate::Hashing {
 #[cfg(test)]
 mod test {
     use std::convert::TryFrom;
-    use std::str;
-    use crate::{Hashing, MessageFormatter};
-    use crate::proto::hashing::{Blake3DeriveKeyRequest, Blake3HashRequest};
+    use crate::{Hashing};
+    use crate::proto::hashing::{Blake3DeriveKeyRequest, Blake3HashRequest, Blake3KeyedHashRequest};
 
     #[test]
     fn test_blake3_hash() {
         let data = "Hello, world!";
-        let request = Blake3HashRequest{data: data.as_bytes().to_vec(), key: None };
+        let request = Blake3HashRequest{data: data.as_bytes().to_vec() };
         let response = Hashing::blake3_hash(&request).unwrap();
         let hash_data = blake3::hash(data.as_bytes());
         assert_eq!(response.output.as_slice(), hash_data.as_bytes())
@@ -56,7 +55,7 @@ mod test {
     fn test_blake3_keyed_hash() {
         let data = "Hello, world!";
         let key_str = format!("{:_<32}", "4113"); // Required to be 32-bytes
-        let request = Blake3HashRequest{data: data.as_bytes().to_vec(), key: Option::from(key_str.as_bytes().to_vec()) };
+        let request = Blake3KeyedHashRequest{data: data.as_bytes().to_vec(), key: key_str.as_bytes().to_vec() };
         let response = Hashing::blake3_keyed_hash(&request).unwrap();
         let hash_data = blake3::keyed_hash(<&[u8; 32]>::try_from(key_str.as_bytes()).unwrap(), data.as_bytes());
         assert_eq!(response.output.as_slice(), hash_data.as_bytes())
@@ -66,7 +65,7 @@ mod test {
     fn test_blake3_keyed_hash_invalid_size() {
         let data = "Hello, world!";
         let key_str = format!("{:_<16}", "4113"); // Required to be 32-bytes
-        let request = Blake3HashRequest{data: data.as_bytes().to_vec(), key: Option::from(key_str.as_bytes().to_vec()) };
+        let request = Blake3KeyedHashRequest{data: data.as_bytes().to_vec(), key: key_str.as_bytes().to_vec() };
         assert!(Hashing::blake3_keyed_hash(&request).is_err());
     }
 
--- native/src/proto/okapi/okapi_hashing.rs
+++ native/src/proto/okapi/okapi_hashing.rs
@@ -3,12 +3,24 @@
 pub struct Blake3HashRequest {
     #[prost(bytes="vec", tag="1")]
     pub data: ::prost::alloc::vec::Vec<u8>,
-    #[prost(bytes="vec", optional, tag="2")]
-    pub key: ::core::option::Option<::prost::alloc::vec::Vec<u8>>,
 }
 #[derive(::serde::Serialize, ::serde::Deserialize)]
 #[derive(Clone, PartialEq, ::prost::Message)]
 pub struct Blake3HashResponse {
+    #[prost(bytes="vec", tag="1")]
+    pub output: ::prost::alloc::vec::Vec<u8>,
+}
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Blake3KeyedHashRequest {
+    #[prost(bytes="vec", tag="1")]
+    pub data: ::prost::alloc::vec::Vec<u8>,
+    #[prost(bytes="vec", tag="2")]
+    pub key: ::prost::alloc::vec::Vec<u8>,
+}
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Blake3KeyedHashResponse {
     #[prost(bytes="vec", tag="1")]
     pub output: ::prost::alloc::vec::Vec<u8>,
 }
