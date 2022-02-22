1a4f738e960f732cb9038736c41f9467f32280df
Scott Phillips <polygonguru@gmail.com>
2022-02-22 10:18:54 -0500

    python, golang, dotnet, ruby linting (#366)

--- python/trinsicokapi/hashing.py
+++ python/trinsicokapi/hashing.py
@@ -1,6 +1,14 @@
+from trinsicokapi.proto.okapi.hashing.v1 import (
+    Sha256HashResponse,
+    Sha256HashRequest,
+    Blake3HashRequest,
+    Blake3HashResponse,
+    Blake3KeyedHashRequest,
+    Blake3KeyedHashResponse,
+    Blake3DeriveKeyRequest,
+    Blake3DeriveKeyResponse,
+)
 from trinsicokapi.wrapper import _typed_wrap_and_call
-
-from trinsicokapi.proto.okapi.hashing.v1 import *
 
 
 def sha256_hash(request: Sha256HashRequest) -> Sha256HashResponse:
@@ -14,10 +22,14 @@ def blake3_hash(request: Blake3HashRequest) -> Blake3HashResponse:
 
 
 def blake3_keyed_hash(request: Blake3KeyedHashRequest) -> Blake3KeyedHashResponse:
-    response = _typed_wrap_and_call("blake3_keyed_hash", request, Blake3KeyedHashResponse)
+    response = _typed_wrap_and_call(
+        "blake3_keyed_hash", request, Blake3KeyedHashResponse
+    )
     return response
 
 
 def blake3_derive_key(request: Blake3DeriveKeyRequest) -> Blake3DeriveKeyResponse:
-    response = _typed_wrap_and_call("blake3_derive_key", request, Blake3DeriveKeyResponse)
+    response = _typed_wrap_and_call(
+        "blake3_derive_key", request, Blake3DeriveKeyResponse
+    )
     return response
