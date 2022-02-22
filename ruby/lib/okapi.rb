ae643e000d6255ca0a1253ad6e9034b61472ecda
Scott Phillips <polygonguru@gmail.com>
2022-02-22 12:33:18 -0500

    rubocop warning fixes

--- ruby/lib/okapi.rb
+++ ruby/lib/okapi.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require_relative 'ld_proofs'
 require 'ffi'
 require 'os'
 require 'okapi/keys/v1/keys_pb'
@@ -15,16 +16,17 @@ Proofs_V1 = Okapi::Proofs::V1
 Security_V1 = Okapi::Security::V1
 Transport_V1 = Okapi::Transport::V1
 
+# Okapi wrapper module
 module Okapi
   extend FFI::Library
-  @@library_path = nil
-  @@library_linked = false
+  @library_path = nil
+  @library_linked = false
   def self.library_path
-    @@library_path
+    @library_path
   end
 
   def self.library_path=(path)
-    @@library_path = path
+    @library_path = path
   end
 
   def self.library_directory
@@ -53,17 +55,20 @@ module Okapi
            :message, :string
   end
 
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
   def self.load_native_library
-    return if @@library_linked
+    return if @library_linked
 
-    @@library_linked = true
+    @library_linked = true
 
     full_path = library_name
     full_path = File.expand_path(File.join(library_path, library_name)) unless library_path.nil?
     begin
       ffi_lib full_path
     rescue LoadError
-      # Get the environment variable RUBY_DLL_PATH on all platforms as a failsafe, MacOS system integrity protection, I'm looking at you.
+      # Get the environment variable RUBY_DLL_PATH on all platforms as a failsafe,
+      # MacOS system integrity protection, I'm looking at you.
       full_path = File.expand_path(File.join(ENV['RUBY_DLL_PATH'], library_name))
       ffi_lib full_path
     end
@@ -95,9 +100,27 @@ module Okapi
     attach_function :okapi_bytebuffer_free, [ByteBuffer.by_value], :int
     attach_function :okapi_string_free, [:pointer], :int
   end
-end
 
-module Okapi
+  def self.ffi_call(function, request, response_klass)
+    load_native_library
+    # static method call: e.g. Okapi::Keys::GenerateKeyRequest.encode(request)
+    serialized = request.class.encode(request)
+    request_buffer = Okapi::ByteBuffer.new
+    request_buffer[:len] = serialized.bytesize
+    request_buffer[:data] = FFI::MemoryPointer.from_string(serialized)
+    response_buffer = Okapi::ByteBuffer.new
+    error = Okapi::ExternError.new
+    extern_function = Okapi.method(function)
+    response_value = extern_function.call(request_buffer, response_buffer, error)
+    raise Okapi::DidError.new(code: response_value, msg: error[:message]) if response_value != 0
+
+    response = response_klass.decode(response_buffer[:data].read_string(response_buffer[:len]))
+    byte_buffer_free(response_buffer)
+    response
+  end
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/AbcSize
+
   def self.verify_type(arg, klass)
     raise "Argument type error is: #{arg.class}, should be #{klass}" unless arg.is_a?(klass)
   end
@@ -112,135 +135,15 @@ module Okapi
     okapi_string_free(ptr)
   end
 
-  def self.ffi_call(function, request, response_klass)
-    load_native_library
-    # static method call: e.g. Okapi::Keys::GenerateKeyRequest.encode(request)
-    serialized = request.class.encode(request)
-    request_buffer = Okapi::ByteBuffer.new
-    request_buffer[:len] = serialized.bytesize
-    request_buffer[:data] = FFI::MemoryPointer.from_string(serialized)
-    response_buffer = Okapi::ByteBuffer.new
-    error = Okapi::ExternError.new
-    extern_function = Okapi.method(function)
-    response_value = extern_function.call(request_buffer, response_buffer, error)
-    if response_value != 0
-      raise Okapi::DidError.new(code: response_value,
-
-                                msg: error[:message])
-    end
-    response = response_klass.decode(response_buffer[:data].read_string(response_buffer[:len]))
-    byte_buffer_free(response_buffer)
-    response
-  end
-
+  # Wrapping error for failures in the native code
   class DidError < StandardError
-    def initialize(code = 0, msg = nil)
+    def initialize(code = 0, msg = '')
       @code = code
       super(msg)
     end
 
     def to_s
       "code=#{@code} message=#{message}"
-    end
-  end
-
-  module DidComm
-    def self.pack(request)
-      Okapi.verify_type(request, Transport_V1::PackRequest)
-      Okapi.ffi_call('didcomm_pack', request, Transport_V1::PackResponse)
-    end
-
-    def self.unpack(request)
-      Okapi.verify_type(request, Transport_V1::UnpackRequest)
-      Okapi.ffi_call('didcomm_unpack', request, Transport_V1::UnpackResponse)
-    end
-
-    def self.sign(request)
-      Okapi.verify_type(request, Transport_V1::SignRequest)
-      Okapi.ffi_call('didcomm_sign', request, Transport_V1::SignResponse)
-    end
-
-    def self.verify(request)
-      Okapi.verify_type(request, Transport_V1::VerifyRequest)
-      Okapi.ffi_call('didcomm_verify', request, Transport_V1::VerifyResponse)
-    end
-  end
-
-  module DidKey
-    def self.generate(request)
-      Okapi.verify_type(request, Keys_V1::GenerateKeyRequest)
-      Okapi.ffi_call('didkey_generate', request, Keys_V1::GenerateKeyResponse)
-    end
-
-    def self.resolve(request)
-      Okapi.verify_type(request, Keys_V1::ResolveRequest)
-      Okapi.ffi_call('didkey_resolve', request, Keys_V1::ResolveResponse)
-    end
-  end
-
-  module LdProofs
-    def self.create(request)
-      Okapi.verify_type(request, Proofs_V1::CreateProofRequest)
-      Okapi.ffi_call('ldproofs_create_proof', request, Proofs_V1::CreateProofResponse)
-    end
-
-    def self.verify(request)
-      Okapi.verify_type(request, Proofs_V1::VerifyProofRequest)
-      Okapi.ffi_call('ldproofs_verify_proof', request, Proofs_V1::VerifyProofResponse)
-    end
-  end
-
-  module Oberon
-    def self.create_key(request)
-      Okapi.verify_type(request, Security_V1::CreateOberonKeyRequest)
-      Okapi.ffi_call('oberon_create_key', request, Security_V1::CreateOberonKeyResponse)
-    end
-
-    def self.create_token(request)
-      Okapi.verify_type(request, Security_V1::CreateOberonTokenRequest)
-      Okapi.ffi_call('oberon_create_token', request, Security_V1::CreateOberonTokenResponse)
-    end
-
-    def self.blind_token(request)
-      Okapi.verify_type(request, Security_V1::BlindOberonTokenRequest)
-      Okapi.ffi_call('oberon_blind_token', request, Security_V1::BlindOberonTokenResponse)
-    end
-
-    def self.unblind_token(request)
-      Okapi.verify_type(request, Security_V1::UnBlindOberonTokenRequest)
-      Okapi.ffi_call('oberon_unblind_token', request, Security_V1::UnBlindOberonTokenResponse)
-    end
-
-    def self.create_proof(request)
-      Okapi.verify_type(request, Security_V1::CreateOberonProofRequest)
-      Okapi.ffi_call('oberon_create_proof', request, Security_V1::CreateOberonProofResponse)
-    end
-
-    def self.verify_proof(request)
-      Okapi.verify_type(request, Security_V1::VerifyOberonProofRequest)
-      Okapi.ffi_call('oberon_verify_proof', request, Security_V1::VerifyOberonProofResponse)
-    end
-  end
-
-  module Hashing
-    def self.blake3_hash(request)
-      Okapi.verify_type(request, Hashing_V1::Blake3HashRequest)
-      Okapi.ffi_call('blake3_hash', request, Hashing_V1::Blake3HashResponse)
-    end
-
-    def self.blake3_keyed_hash(request)
-      Okapi.verify_type(request, Hashing_V1::Blake3KeyedHashRequest)
-      Okapi.ffi_call('blake3_keyed_hash', request, Hashing_V1::Blake3KeyedHashResponse)
-    end
-
-    def self.blake3_derive_key(request)
-      Okapi.verify_type(request, Hashing_V1::Blake3DeriveKeyRequest)
-      Okapi.ffi_call('blake3_derive_key', request, Hashing_V1::Blake3DeriveKeyResponse)
-    end
-
-    def self.sha256_hash(request)
-      Okapi.verify_type(request, Hashing_V1::SHA256HashRequest)
-      Okapi.ffi_call('sha256_hash', request, Hashing_V1::SHA256HashResponse)
     end
   end
 end
--- ruby/test/key_test.rb
+++ ruby/test/key_test.rb
@@ -4,6 +4,7 @@ require_relative 'test_helper'
 require 'okapi/keys/v1/keys_pb'
 require 'okapi/security/v1/security_pb'
 require 'okapi'
+require 'did_key'
 require 'version'
 require 'base64'
 require 'base58'
@@ -18,7 +19,7 @@ class KeyTest < Minitest::Test
                                               seed: "\x01\x02\x03")
     assert !request.nil?
 
-    response = Okapi::DidKey.generate(request)
+    response = DidKey.generate(request)
     assert !response.nil?
     assert response.is_a?(Keys_V1::GenerateKeyResponse)
   end
@@ -26,14 +27,14 @@ class KeyTest < Minitest::Test
   def test_generate_key_no_seed
     request = Keys_V1::GenerateKeyRequest.new
     request.key_type = Keys_V1::KeyType::KEY_TYPE_ED25519
-    response = Okapi::DidKey.generate(request)
+    response = DidKey.generate(request)
 
     assert_valid_key_generated(response)
   end
 
   def test_resolve_key
     key = 'did:key:z6Mkt6QT8FPajKXDrtMefkjxRQENd9wFzKkDFomdQAVFzpzm#z6LSfDq6DuofPeZUqNEmdZsxpvfHvSoUXGEWFhw7JHk4cynN'
-    response = Okapi::DidKey.resolve(Keys_V1::ResolveRequest.new(did: key))
+    response = DidKey.resolve(Keys_V1::ResolveRequest.new(did: key))
     assert !response.nil?
   end
 
@@ -41,41 +42,26 @@ class KeyTest < Minitest::Test
     request = Keys_V1::GenerateKeyRequest.new
     request.key_type = -1
     assert_raises(Okapi::DidError) do
-      Okapi::DidKey.generate(request)
+      DidKey.generate(request)
     end
   end
 
-  def test_generate_key_from_seed_Ed25519
+  def test_generate_key_from_seed
     type = Keys_V1::KeyType::KEY_TYPE_ED25519
     type_string = 'Ed25519'
     seed = '4f66b355aa7b0980ff901f2295b9c562ac3061be4df86703eb28c612faae6578'
     expected_response = '6fioC1zcDPyPEL19pXRS2E4iJ46zH7xP6uSgAaPdwDrx'
-    request = Keys_V1::GenerateKeyRequest.new
-    request.key_type = type
-    request.seed = [seed].pack('H*')
+    assert_key_generated(seed, type, type_string, expected_response)
 
-    response = Okapi::DidKey.generate(request)
-    public_key = assert_valid_key_generated(response, type_string)
-
-    assert_equal(expected_response, Base58.binary_to_base58(public_key, :bitcoin))
-  end
-
-  def test_generate_key_from_seed_X25519
     type = Keys_V1::KeyType::KEY_TYPE_X25519
     type_string = 'X25519'
     seed = '9b29d42b38ddd52ed39c0ff70b39572a6eb9b3cac201918dc6d6a84b4c88d2a5'
     expected_response = '3EK9AYXoUV4Unn5AjvYY39hyK91n7gg4ExC8rKKSUQXJ'
-
-    request = Keys_V1::GenerateKeyRequest.new
-    request.key_type = type
-    request.seed = [seed].pack('H*')
-
-    response = Okapi::DidKey.generate(request)
-    public_key = assert_valid_key_generated(response, type_string)
-
-    assert_equal(expected_response, Base58.binary_to_base58(public_key, :bitcoin))
+    assert_key_generated(seed, type, type_string, expected_response)
   end
 
+  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics/AbcSize
   def assert_valid_key_generated(response, crv = 'Ed25519')
     assert !response.nil?
     assert !response.key[0].nil?
@@ -85,20 +71,30 @@ class KeyTest < Minitest::Test
     public_key = x + y
     assert !public_key.nil?
     assert public_key.length == 32
-    response_64 = Base64.urlsafe_decode64(base64_padding(response.key[0].d))
-    assert !response_64.nil?
-    assert response_64.length == 32
+    response64 = Base64.urlsafe_decode64(base64_padding(response.key[0].d))
+    assert !response64.nil?
+    assert response64.length == 32
     public_key
   end
+  # rubocop:enable Metrics/MethodLength
+  # rubocop:enable Metrics/AbcSize
 
-  def base64_padding(base_64)
-    string_short = base_64.length % 4
-    case string_short
-    when 2
-      base_64 += '=='
-    when 3
-      base_64 += '='
-    end
-    base_64
+  def base64_padding(base64)
+    string_short = base64.length % 4
+    base64 += '==' if string_short == 2
+    base64 += '=' if string_short == 3
+    base64
+  end
+
+  private
+
+  def assert_key_generated(seed, type, type_string, expected_response)
+    request = Keys_V1::GenerateKeyRequest.new
+    request.key_type = type
+    request.seed = [seed].pack('H*')
+
+    response = DidKey.generate(request)
+    public_key = assert_valid_key_generated(response, type_string)
+    assert_equal(expected_response, Base58.binary_to_base58(public_key, :bitcoin))
   end
 end
