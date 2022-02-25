b26f8fbb36c816fbe44fcd6de586c133cc6042dc
Tomislav Markovski <tmarkovski@gmail.com>
2022-02-25 13:33:58 -0500

    Cargo fmt

--- native/src/proto/okapi/mod.rs
+++ native/src/proto/okapi/mod.rs
@@ -1,7 +1,6 @@
 pub mod okapi_examples;
+pub mod okapi_hashing;
 pub mod okapi_keys;
 pub mod okapi_proofs;
 pub mod okapi_security;
 pub mod okapi_transport;
-pub mod okapi_security;
-pub mod okapi_hashing;
