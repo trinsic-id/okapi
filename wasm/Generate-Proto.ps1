3dcbc32bf0c536d6bfef33e01196f404ed1a1786
Scott Phillips <polygonguru@gmail.com>
2022-05-04 17:57:32 -0400

    switch to `ts-proto` building

--- wasm/Generate-Proto.ps1
+++ wasm/Generate-Proto.ps1
@@ -1,30 +1,25 @@
 # Generate protobuf files for JS and TS
-param($OutDir = './packages/okapi-proto/src/proto')
+param($OutDir = "$PSScriptRoot/packages/okapi-proto/src/proto")
 
-# Windows
-if ($IsWindows)
-{
-    $PROTOC_GEN_TS_PATH = Resolve-Path "./node_modules/.bin/protoc-gen-ts.cmd"
-    $GRPC_TOOLS_NODE_PROTOC = Resolve-Path "./node_modules/.bin/grpc_tools_node_protoc.cmd"
-}
-else
-{
-    $PROTOC_GEN_TS_PATH = Resolve-Path "./node_modules/.bin/protoc-gen-ts"
-    $GRPC_TOOLS_NODE_PROTOC = Resolve-Path "./node_modules/.bin/grpc_tools_node_protoc"
-}
+$PROTOC_GEN_TS_PATH = Resolve-Path "$PSScriptRoot/node_modules/.bin/protoc-gen-ts_proto$(If ($IsWindows) {'.cmd'} Else {''})"
+# For mac, `brew install protobuf`
+# for windows, download and install to PATH
+# for linux, `apt-get install protobuf`
+$PROTOC = "protoc"
 $OUTPUT_DIR = $OutDir
-$PROTO_DIR = Resolve-Path "../proto"
+$PROTO_DIR = Resolve-Path "$PSScriptRoot/../proto"
+Write-Output $PROTO_DIR
+
+if ((Get-Command "$PROTOC" -ErrorAction SilentlyContinue) -eq $null)
+{
+    Write-Host "Unable to find $PROTOC in your PATH"
+    Return
+}
 
 foreach ($Item in Get-ChildItem -Path $PROTO_DIR -Include *.proto -Recurse)
 {
     $File = $Item.FullName
-
-    # If you want to run in the same powershell window, use this:
-    # &"$GRPC_TOOLS_NODE_PROTOC" --plugin=protoc-gen-ts=$PROTOC_GEN_TS_PATH --js_out=import_style=commonjs,binary:$OUTPUT_DIR --ts_out=$OUTPUT_DIR -I $PROTO_DIR $File
-
-    # JavaScript code generating
-    &"$GRPC_TOOLS_NODE_PROTOC" --js_out=import_style=commonjs,binary:$OUTPUT_DIR --grpc_out=grpc_js:$OUTPUT_DIR -I $PROTO_DIR $File
-
-    # TypeScript definitions
-    &"$GRPC_TOOLS_NODE_PROTOC" --plugin=protoc-gen-ts=$PROTOC_GEN_TS_PATH --ts_out=grpc_js:$OUTPUT_DIR -I $PROTO_DIR $File
+    $Expr = "$PROTOC --plugin=protoc-gen-ts=$PROTOC_GEN_TS_PATH --ts_proto_out=$OUTPUT_DIR --ts_proto_opt=esModuleInterop=true,outputServices=generic-definitions,useExactTypes=false,exportCommonSymbols=false -I $PROTO_DIR $File"
+    Write-Output $Expr
+    Invoke-Expression $Expr
 }
--- wasm/packages/okapi/test/node.test.js
+++ wasm/packages/okapi/test/node.test.js
@@ -2,38 +2,46 @@ const test = require("ava");
 const {
   DIDKey,
   GenerateKeyRequest,
+  Blake3HashRequest,
   CreateOberonKeyRequest,
   CreateOberonTokenRequest,
   CreateOberonProofRequest,
   VerifyOberonProofRequest,
   KeyType,
   Oberon,
+  Hashing,
 } = require("../lib/index.js");
 
 test("generate bls key", async (t) => {
-  var response = await DIDKey.generate(new GenerateKeyRequest().setKeyType(KeyType.KEY_TYPE_BLS12381G1G2));
+  const response = await DIDKey.generate(new GenerateKeyRequest().setKeyType(KeyType.KEY_TYPE_BLS12381G1G2));
 
-  t.not(null, response);
-  t.not(undefined, response);
+  t.not(response, null);
+  t.not(response, undefined);
 });
 
 test("create and verify oberon token", async (t) => {
-  var key = await Oberon.createKey(new CreateOberonKeyRequest());
-  var id = Buffer.from("me@example.com");
-  var nonce = Buffer.from("123");
+  const key = await Oberon.createKey(new CreateOberonKeyRequest());
+  const id = Buffer.from("me@example.com");
+  const nonce = Buffer.from("123");
 
-  var token = await Oberon.createToken(new CreateOberonTokenRequest().setData(id).setSk(key.getSk()));
-  t.not(null, token);
+  const token = await Oberon.createToken(new CreateOberonTokenRequest().setData(id).setSk(key.getSk()));
+  t.not(token, null);
 
-  var proof = await Oberon.createProof(
-    new CreateOberonProofRequest().setToken(token.getToken()).setData(id).setNonce(nonce)
+  const proof = await Oberon.createProof(
+      new CreateOberonProofRequest().setToken(token.getToken()).setData(id).setNonce(nonce)
   );
 
-  t.is(256, proof.getProof_asU8().length);
+  t.is(proof.getProof_asU8().length, 256);
 
-  var result = await Oberon.verifyProof(
-    new VerifyOberonProofRequest().setData(id).setNonce(nonce).setProof(proof.getProof()).setPk(key.getPk())
+  const result = await Oberon.verifyProof(
+      new VerifyOberonProofRequest().setData(id).setNonce(nonce).setProof(proof.getProof()).setPk(key.getPk())
   );
 
   t.true(result.getValid());
 });
+
+test("run blake3 hash", async (t) => {
+  const response = await Hashing.blake3Hash(new Blake3HashRequest().setData([1,2,3]));
+  t.not(response, null);
+  t.not(response, undefined);
+})
