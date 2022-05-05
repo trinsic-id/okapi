2a9cfd699a2199a8b28490e63fdfb98c1cd86bde
Scott Phillips <polygonguru@gmail.com>
2022-05-05 10:05:26 -0400

    wasm wrapper files, but not the binary wasm file

---
+++ wasm/packages/okapi/src/native/okapi_wasm.d.ts
@@ -0,0 +1,92 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function didkey_generate(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function didkey_resolve(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function blake3_hash(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function blake3_keyed_hash(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function blake3_derive_key(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function sha256_hash(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function didcomm_pack(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function didcomm_unpack(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function didcomm_sign(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function didcomm_verify(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function ldproofs_create_proof(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function ldproofs_verify_proof(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function oberon_create_key(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function oberon_create_token(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function oberon_create_proof(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function oberon_verify_proof(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function oberon_blind_token(request: Uint8Array): Uint8Array;
+/**
+* @param {Uint8Array} request
+* @returns {Uint8Array}
+*/
+export function oberon_unblind_token(request: Uint8Array): Uint8Array;
---
+++ wasm/packages/okapi/src/native/okapi_wasm_bg.wasm.d.ts
@@ -0,0 +1,22 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export function didkey_generate(a: number): number;
+export function didkey_resolve(a: number): number;
+export function blake3_hash(a: number): number;
+export function blake3_keyed_hash(a: number): number;
+export function blake3_derive_key(a: number): number;
+export function sha256_hash(a: number): number;
+export function didcomm_pack(a: number): number;
+export function didcomm_unpack(a: number): number;
+export function didcomm_sign(a: number): number;
+export function didcomm_verify(a: number): number;
+export function ldproofs_create_proof(a: number): number;
+export function ldproofs_verify_proof(a: number): number;
+export function oberon_create_key(a: number): number;
+export function oberon_create_token(a: number): number;
+export function oberon_create_proof(a: number): number;
+export function oberon_verify_proof(a: number): number;
+export function oberon_blind_token(a: number): number;
+export function oberon_unblind_token(a: number): number;
+export function __wbindgen_exn_store(a: number): void;
