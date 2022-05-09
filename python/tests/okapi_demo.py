1740100b89f4322dc6b172e3b88c25d57e48a081
Scott Phillips <polygonguru@gmail.com>
2022-05-09 09:48:44 -0400

    random python demo update

--- python/tests/okapi_demo.py
+++ python/tests/okapi_demo.py
@@ -1,9 +1,9 @@
-from trinsicokapi.wrapper import DIDKey
+from trinsicokapi import didkey
 from trinsicokapi.proto.okapi.keys.v1 import GenerateKeyRequest, KeyType
 
 request = GenerateKeyRequest()
-request.key_type = KeyType.Ed25519
+request.key_type = KeyType.KEY_TYPE_ED25519
 request.seed = bytes(bytearray([1, 2, 3]))
 
-key_response = DIDKey.generate(request)
+key_response = didkey.generate(request)
 print(key_response.key)
