bab8ef6a9fa2d2c52e6188846925203e5a6a22e1
Scott Phillips <polygonguru@gmail.com>
2022-05-11 19:51:32 -0400

    conditional compile for windows (#407)

--- go/okapi/native_call_windows.go
+++ go/okapi/native_call_windows.go
@@ -13,7 +13,7 @@ func callOkapiNative(request proto.Message, response proto.Message, funcName str
 	}
 	dll := syscall.MustLoadDLL(getLibraryName())
 	okapiFunc := dll.MustFindProc(funcName)
-	code, _, err := okapiFunc.Call(uintptr(unsafe.Pointer(&requestBuffer)), uintptr(unsafe.Pointer(&responseBuffer)), uintptr(unsafe.Pointer(&errorBuffer)))
+	_, _, err = okapiFunc.Call(uintptr(unsafe.Pointer(&requestBuffer)), uintptr(unsafe.Pointer(&responseBuffer)), uintptr(unsafe.Pointer(&errorBuffer)))
 	if err != syscall.Errno(0x0) {
 		// Actually check the syscall.Errno to see if it's a real error
 		return err
@@ -22,13 +22,13 @@ func callOkapiNative(request proto.Message, response proto.Message, funcName str
 	if err != nil {
 		return wrapError("Failed to unmarshal response", err)
 	}
-	return createError(int32(code), errorBuffer)
+	return createError(errorBuffer)
 }
 
 func byteBufferFree(responseBuffer ByteBuffer) error {
 	dll := syscall.MustLoadDLL("okapi.dll")
 	okapiFunc := dll.MustFindProc("okapi_bytebuffer_free")
-	code, _, err := okapiFunc.Call(uintptr(unsafe.Pointer(&responseBuffer)))
+	_, _, err := okapiFunc.Call(uintptr(unsafe.Pointer(&responseBuffer)))
 	if err != syscall.Errno(0x0) {
 		// Actually check the syscall.Errno to see if it's a real error
 		return err
