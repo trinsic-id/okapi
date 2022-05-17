53a273f6c52948adaf30aea70236e2500f3fbbf5
Scott Phillips <polygonguru@gmail.com>
2022-05-17 13:37:58 -0400

    no message

--- native/src/proto/okapi/okapi_security.rs
+++ native/src/proto/okapi/okapi_security.rs
@@ -1,118 +1,130 @@
 /// Create an Oberon Compatible Secret Key
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct CreateOberonKeyRequest {
     /// optional seed to generate deterministic keys
-    #[prost(bytes = "vec", tag = "1")]
+    #[prost(bytes="vec", tag="1")]
     pub seed: ::prost::alloc::vec::Vec<u8>,
 }
 /// Contains the oberon secret key bytes
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct CreateOberonKeyResponse {
     /// raw secret key bytes
-    #[prost(bytes = "vec", tag = "2")]
+    #[prost(bytes="vec", tag="2")]
     pub sk: ::prost::alloc::vec::Vec<u8>,
     /// raw public key bytes
-    #[prost(bytes = "vec", tag = "3")]
+    #[prost(bytes="vec", tag="3")]
     pub pk: ::prost::alloc::vec::Vec<u8>,
 }
 /// Create a new oberon token
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct CreateOberonTokenRequest {
     /// raw BLS key bytes
-    #[prost(bytes = "vec", tag = "1")]
+    #[prost(bytes="vec", tag="1")]
     pub sk: ::prost::alloc::vec::Vec<u8>,
     /// data is the public part of the oberon protocol and can be any data
-    #[prost(bytes = "vec", tag = "2")]
+    #[prost(bytes="vec", tag="2")]
     pub data: ::prost::alloc::vec::Vec<u8>,
     /// optional blinding for the token
-    #[prost(bytes = "vec", repeated, tag = "3")]
+    #[prost(bytes="vec", repeated, tag="3")]
     pub blinding: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
 }
-/// Contains the token with optional blinding
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+/// Contains the token with optional blinding 
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct CreateOberonTokenResponse {
     /// raw token bytes
-    #[prost(bytes = "vec", tag = "1")]
+    #[prost(bytes="vec", tag="1")]
     pub token: ::prost::alloc::vec::Vec<u8>,
 }
 /// Create a proof that holder knows the token
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct CreateOberonProofRequest {
     /// data used to create the token
-    #[prost(bytes = "vec", tag = "1")]
+    #[prost(bytes="vec", tag="1")]
     pub data: ::prost::alloc::vec::Vec<u8>,
     /// token data
-    #[prost(bytes = "vec", tag = "2")]
+    #[prost(bytes="vec", tag="2")]
     pub token: ::prost::alloc::vec::Vec<u8>,
     /// any blindings used to create the token
-    #[prost(bytes = "vec", repeated, tag = "3")]
+    #[prost(bytes="vec", repeated, tag="3")]
     pub blinding: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
     /// nonce for generating the proof
-    #[prost(bytes = "vec", tag = "4")]
+    #[prost(bytes="vec", tag="4")]
     pub nonce: ::prost::alloc::vec::Vec<u8>,
 }
 /// Contains the token proof
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct CreateOberonProofResponse {
     /// raw proof bytes
-    #[prost(bytes = "vec", tag = "2")]
+    #[prost(bytes="vec", tag="2")]
     pub proof: ::prost::alloc::vec::Vec<u8>,
 }
 /// Verify the presented proof is valid
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct VerifyOberonProofRequest {
     /// raw proof bytes returned from CreateProof
-    #[prost(bytes = "vec", tag = "1")]
+    #[prost(bytes="vec", tag="1")]
     pub proof: ::prost::alloc::vec::Vec<u8>,
     /// data used to create the token
-    #[prost(bytes = "vec", tag = "2")]
+    #[prost(bytes="vec", tag="2")]
     pub data: ::prost::alloc::vec::Vec<u8>,
     /// nonce used to generate the proof
-    #[prost(bytes = "vec", tag = "3")]
+    #[prost(bytes="vec", tag="3")]
     pub nonce: ::prost::alloc::vec::Vec<u8>,
     /// public key that was used to generate the token
-    #[prost(bytes = "vec", tag = "4")]
+    #[prost(bytes="vec", tag="4")]
     pub pk: ::prost::alloc::vec::Vec<u8>,
 }
 /// Contains the status of the proof validation
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct VerifyOberonProofResponse {
     /// whether the given proof was valid
-    #[prost(bool, tag = "1")]
+    #[prost(bool, tag="1")]
     pub valid: bool,
 }
-/// Blind an oberon token
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+/// Blind an oberon token 
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct BlindOberonTokenRequest {
     /// raw token bytes
-    #[prost(bytes = "vec", tag = "1")]
+    #[prost(bytes="vec", tag="1")]
     pub token: ::prost::alloc::vec::Vec<u8>,
     /// blinding to apply to the token
-    #[prost(bytes = "vec", repeated, tag = "2")]
+    #[prost(bytes="vec", repeated, tag="2")]
     pub blinding: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
 }
 /// Contains the blinded token reply
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct BlindOberonTokenResponse {
     /// raw blinded token bytes
-    #[prost(bytes = "vec", tag = "1")]
+    #[prost(bytes="vec", tag="1")]
     pub token: ::prost::alloc::vec::Vec<u8>,
 }
-/// UnBlind an oberon token
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+/// UnBlind an oberon token 
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct UnBlindOberonTokenRequest {
     /// raw token bytes
-    #[prost(bytes = "vec", tag = "1")]
+    #[prost(bytes="vec", tag="1")]
     pub token: ::prost::alloc::vec::Vec<u8>,
     /// blinding to remove from the token
-    #[prost(bytes = "vec", repeated, tag = "2")]
+    #[prost(bytes="vec", repeated, tag="2")]
     pub blinding: ::prost::alloc::vec::Vec<::prost::alloc::vec::Vec<u8>>,
 }
 /// Contains the unblinded token reply
-#[derive(::serde::Serialize, ::serde::Deserialize, Clone, PartialEq, ::prost::Message)]
+#[derive(::serde::Serialize, ::serde::Deserialize)]
+#[derive(Clone, PartialEq, ::prost::Message)]
 pub struct UnBlindOberonTokenResponse {
     /// raw unblinded token bytes
-    #[prost(bytes = "vec", tag = "1")]
+    #[prost(bytes="vec", tag="1")]
     pub token: ::prost::alloc::vec::Vec<u8>,
 }
 /// Verify that an oberon token comes from the desired issuer
